77bdb17c845430a6e9a5f856455b139609d97853
Joël Perras <joel.perras@gmail.com>
2009-10-26 09:41:56 -0400

    Updating class doc block & other doc blocks for Memory cache adapter.

--- libraries/lithium/storage/cache/adapters/Memory.php
+++ libraries/lithium/storage/cache/adapters/Memory.php
@@ -2,16 +2,36 @@
 
 namespace lithium\storage\cache\adapters;
 
-class Memory extends \lithium\core\Adaptable {
+/**
+ * A minimal in-memory cache.
+ *
+ * This Memory adapter provides basic support for `write`, `read`, `delete`
+ * and `clear` cache functionality, as well as allowing the first four
+ * methods to be filtered as per the Lithium filtering system.
+ *
+ * This cache adapter does not implement any expiry-based cache invalidation
+ * logic, as the cached data will only persist for the lifetime of the current request.
+ *
+ * As a result, this cache adapter is best suited for generic memoization of data, and
+ * should not be used for for anything that must persist longer than the current
+ * request cycle.
+ */
+class Memory extends \lithium\core\Object {
 
+	/**
+	 * Array used to store cached data by this adapter
+	 *
+	 * @var array
+	 */
 	protected $_cache = array();
 
 	/**
-	 * Reads data from $key
+	 * Read value(s) from the cache
 	 *
-	 * @param string $key
-	 * @param mixed  $conditions
-	 * @return mixed
+	 * @param string $key        The key to uniquely identify the cached item
+	 * @param object $conditions Conditions under which the operation should proceed
+	 * @return mixed Cached value if successful, false otherwise
+	 * @todo Refactor to use RES_NOTFOUND for return value checks
 	 */
 	public function read($key, $conditions = null) {
 		$cache =& $this->_cache;
@@ -23,12 +43,12 @@ class Memory extends \lithium\core\Adaptable {
 	}
 
 	/**
-	 * Writes $data to $key
+	 * Write value(s) to the cache
 	 *
-	 * @param string $key
-	 * @param mixed  $data
-	 * @param mixed  $conditions
-	 * @return boolean
+	 * @param string $key        The key to uniquely identify the cached item
+	 * @param mixed  $value      The value to be cached
+	 * @param object $conditions Conditions under which the operation should proceed
+	 * @return boolean True on successful write, false otherwise
 	 */
 	public function write($key, $data, $conditions = null) {
 		$cache =& $this->_cache;
@@ -40,11 +60,11 @@ class Memory extends \lithium\core\Adaptable {
 	}
 
 	/**
-	 * Delete a key
+	 * Delete value from the cache
 	 *
-	 * @param string $key
-	 * @param mixed  $conditions
-	 * @return boolean
+	 * @param string $key        The key to uniquely identify the cached item
+	 * @param object $conditions Conditions under which the operation should proceed
+	 * @return mixed True on successful delete, false otherwise
 	 */
 	public function delete($key, $conditions = null) {
 		$cache =& $this->_cache;
@@ -61,9 +81,9 @@ class Memory extends \lithium\core\Adaptable {
 	}
 
 	/**
-	 * Clear all keys
+	 * Clears user-space cache
 	 *
-	 * @return boolean
+	 * @return mixed True on successful clear, false otherwise
 	 */
 	public function clear() {
 		unset($this->_cache);
@@ -71,9 +91,9 @@ class Memory extends \lithium\core\Adaptable {
 	}
 
 	/**
-	 * GC is not enabled for this adapter
+	 * Garbage collection (GC) is not enabled for this adapter
 	 *
-	 * @return boolean
+	 * @return boolean False
 	 */
 	public function clean() {
 		return false;
