6b6ab8ce99d58281b59917d1d594331322e02d2d
gwoo <gwoohoo@gmail.com>
2009-11-05 15:13:32 -0800

    adding some missing tests

---
+++ libraries/lithium/tests/cases/data/model/DocumentTest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Lithium: the most rad php framework
+ *
+ * @copyright     Copyright 2009, Union of RAD (http://union-of-rad.org)
+ * @license       http://opensource.org/licenses/bsd-license.php The BSD License
+ */
+
+namespace lithium\tests\cases\data\model;
+
+use \lithium\data\model\Document;
+
+class DocumentPost extends \lithium\data\Model {
+
+}
+
+class DocumentTest extends \lithium\test\Unit {
+	
+}
+?>
