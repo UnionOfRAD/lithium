72045d59cfe3fefb8c35ac30bcfb420d34560488
Joël Perras <joel.perras@gmail.com>
2009-11-09 16:56:51 -0500

    Adding doc blocks for Php session adapter.

--- libraries/lithium/storage/session/adapters/Php.php
+++ libraries/lithium/storage/session/adapters/Php.php
@@ -8,20 +8,43 @@
 
 namespace lithium\storage\session\adapters;
 
+/**
+ * A minimal adapter to interface with native PHP sessions.
+ *
+ * This adapter provides basic support for `write`, `read` and `delete`
+ * session handling, as well as allowing these three methods to be filtered as
+ * per the Lithium filtering system.
+ *
+ */
 class Php extends \lithium\core\Object {
 
+	/**
+	 * Default ini settings for this session adapter
+	 *
+	 * @var array Keys are session ini settings, but without the `session.` namespace.
+	 */
 	protected $_defaults = array(
 		'name' => '', 'cookie_lifetime' => '86400', 'cookie_domain' => '',
 		'save_path' => '/tmp', 'cookie_secure' => false, 'cookie_httponly' => false
 	);
 
+	/**
+	 * Class constructor
+	 *
+	 * Takes care of setting appropriate configurations for
+	 * this object.
+	 *
+	 * @return void
+	 */
 	public function __construct($config = array()) {
 		parent::__construct((array)$config + $this->_defaults);
 	}
 
 	/**
-	 * Initialization of the session
+	 * Initialization of the session.
 	 *
+	 * @todo Split up into an _initialize() and a _startup().
+	 * @return void
 	 */
 	protected function _init() {
 		session_write_close();
@@ -38,18 +61,35 @@ class Php extends \lithium\core\Object {
 				ini_set("session.$key", $this->_config[$key]);
 			}
 		}
-
 		$_SESSION['_timestamp'] = time();
 	}
 
+	/**
+	 * Obtain the status of the session
+	 *
+	 * @return boolean True if $_SESSION is accessible and if a '_timestamp' key
+	 *                 has been set, false otherwise.
+	 */
 	public function isStarted() {
 		return (isset($_SESSION) && isset($_SESSION['_timestamp']));
 	}
 
+	/**
+	 * Obtain the session id
+	 *
+	 * @return mixed Session id, or null if the session has not been started.
+	 */
 	public function key() {
 		return ($id = session_id()) == '' ? null : $id;
 	}
 
+	/**
+	 * Read value from the session
+	 *
+	 * @param string $key        Key of the entry to be read
+	 * @param array  $options    Options array
+	 * @return mixed             Data in the session if successful, false otherwise
+	 */
 	public function read($key, $options = array()) {
 		return function($self, $params, $chain) {
 			extract($params);
@@ -57,6 +97,14 @@ class Php extends \lithium\core\Object {
 		};
 	}
 
+	/**
+	 * Write value to the session
+	 *
+	 * @param  string $key        Key of the item to be stored
+	 * @param  mixed  $value      The value to be stored
+	 * @param  array  $options    Options array
+	 * @return boolean            True on successful write, false otherwise
+	 */
 	public static function write($key, $value, $options = array()) {
 		return function($self, $params, $chain) {
 			extract($params);
@@ -64,6 +112,13 @@ class Php extends \lithium\core\Object {
 		};
 	}
 
+	/**
+	 * Delete value from the session
+	 *
+	 * @param  string  $key		The key to be deleted
+	 * @param  array   $options Options array
+	 * @return boolean          True on successful delete, false otherwise
+	 */
 	public static function delete($key, $options = array()) {
 		return function($self, $params, $chain) {
 			extract($params);
