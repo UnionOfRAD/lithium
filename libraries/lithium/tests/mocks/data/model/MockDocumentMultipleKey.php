c6cc14c850497e05e0829335b44915d1b2163bf1
gwoo <gwoohoo@gmail.com>
2009-11-16 10:20:15 -0800

    adding missing mock

---
+++ libraries/lithium/tests/mocks/data/model/MockDocumentMultipleKey.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Lithium: the most rad php framework
+ *
+ * @copyright     Copyright 2009, Union of RAD (http://union-of-rad.org)
+ * @license       http://opensource.org/licenses/bsd-license.php The BSD License
+ */
+
+namespace lithium\tests\mocks\data\model;
+
+use \lithium\data\model\Document;
+
+class MockDocumentMultipleKey extends \lithium\data\Model {
+	
+	protected $_meta = array(
+		'key' => array('id', 'rev')
+	);
+	
+	public static function __init($options = array()) {}
+
+	public function ret($record, $param1 = null, $param2 = null) {
+		if ($param2) {
+			return $param2;
+		}
+		if ($param1) {
+			return $param1;
+		}
+		return null;
+	}
+
+	public static function find($type = 'all', $options = array()) {
+		switch ($type) {
+			case 'first':
+				return new Document(array('items' =>
+					array('id' => 2, 'rev' => '1-1', 'name' => 'Two', 'content' => 'Lorem ipsum two')
+				));
+			break;
+			case 'all':
+			default :
+				return new Document(array('items' => array(
+					array('id' => 1, 'rev' => '1-1','name' => 'One', 'content' => 'Lorem ipsum one'),
+					array('id' => 2, 'rev' => '1-1','name' => 'Two', 'content' => 'Lorem ipsum two'),
+					array('id' => 3, 'rev' => '1-1', 'name' => 'Three', 'content' => 'Lorem ipsum three')
+				)));
+			break;
+		}
+	}
+}
