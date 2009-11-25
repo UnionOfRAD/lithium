1d1d920818017eee39d10281f5e1db0d6c533348
James Logsdon <dwarf@girsbrain.org>
2009-11-25 20:59:38 +0000

    98% coverage for `core\Object`

---
+++ libraries/lithium/tests/mocks/core/MockObjectConfiguration.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Lithium: the most rad php framework
+ *
+ * @copyright     Copyright 2009, Union of RAD (http://union-of-rad.org)
+ * @license       http://opensource.org/licenses/bsd-license.php The BSD License
+ */
+
+namespace lithium\tests\mocks\core;
+
+class MockObjectConfiguration extends \lithium\core\Object {
+	protected $_testScalar = 'default';
+	protected $_testArray = array('default');
+
+	public function __construct($config = array()) {
+		if (isset($config['autoConfig'])) {
+			$this->_autoConfig = (array)$config['autoConfig'];
+			unset($config['autoConfig']);
+		}
+		parent::__construct($config);
+	}
+
+	public function testScalar($value) {
+		$this->_testScalar = 'called';
+	}
+
+	public function getConfig() {
+		return array(
+			'testScalar' => $this->_testScalar,
+			'testArray' => $this->_testArray
+		);
+	}
+}
