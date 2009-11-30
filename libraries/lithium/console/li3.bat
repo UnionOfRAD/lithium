7c442bf2433230b63b3b6d4b2eea2ac2ac641c44
David Persson <davidpersson@gmx.de>
2009-11-30 22:57:40 +0100

    Adding windows console wrapper.

---
+++ libraries/lithium/console/li3.bat
@@ -0,0 +1,8 @@
+@echo off
+REM
+REM Lithium: the most rad php framework
+REM
+REM @copyright     Copyright 2009, Union of RAD (http://union-of-rad.org)
+REM @license       http://opensource.org/licenses/bsd-license.php The BSD License
+REM
+php -f %~dp0lithium.php %*
