472d5902175bc909ac1bec6d2c6122c7b1d350e9
David Persson <davidpersson@gmx.de>
2009-12-05 14:26:28 +0100

    Updating layout and home page.

--- app/views/pages/home.html.php
+++ app/views/pages/home.html.php
@@ -6,18 +6,12 @@
  * @license       http://opensource.org/licenses/bsd-license.php The BSD License
  */
 ?>
-<h2>Lithium is coming...</h2>
-
+<h2>Home</h2>
 <p>
-	temporary home page that will eventually be filled with configuration checks.
+	Temporary home page that will eventually be filled with configuration checks.
 </p>
-
-<p>
-	<a href="http://rad-dev.org/lithium/wiki">Lithium Wiki</a>
-</p>
-<p>
-	<a href="http://rad-dev.org/lithium">Lithium Source</a>
-</p>
-<p>
-	<a href="irc://irc.freenode.net/#li3">#li3 irc channel</a>
-</p>
+<ul>
+	<li><a href="http://rad-dev.org/lithium/wiki">Lithium Wiki</a></li>
+	<li><a href="http://rad-dev.org/lithium">Lithium Source</a></li>
+	<li><a href="irc://irc.freenode.net/#li3">#li3 irc channel</a></li>
+</ul>
