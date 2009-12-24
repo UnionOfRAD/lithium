31d7b58b4047a025ee6de7d521d4493c0d8cffbd
gwoo <gwoohoo@gmail.com>
2009-12-24 10:13:38 -0800

    moving \command\Docs to li3_docs

--- libraries/lithium/console/template/docs/header.txt.php
+++
@@ -1,6 +0,0 @@
-/**
- * Lithium: the most rad php framework
- *
- * @copyright     Copyright 2009, Union of RAD (http://union-of-rad.org)
- * @license       http://opensource.org/licenses/bsd-license.php The BSD License
- */
