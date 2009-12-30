60ac5b5c03cdb95bc4dd83195c96bfa696919c02
Joël Perras <joel.perras@gmail.com>
2009-12-30 17:05:11 -0500

    Adding basic Redis cache adapter.
    This adapter uses the phpredis PHP extension (http://github.com/owlient/phpredis), and aims
    to implement only the functionality required for a simple semi-persistent cache.

---
+++ libraries/lithium/storage/cache/adapter/Redis.php
@@ -0,0 +1,204 @@
+<?php
+/**
+ * Lithium: the most rad php framework
+ *
+ * @copyright     Copyright 2009, Union of RAD (http://union-of-rad.org)
+ * @license       http://opensource.org/licenses/bsd-license.php The BSD License
+ */
+
+namespace lithium\storage\cache\adapter;
+
+/**
+ * A Redis (phpredis) cache adapter implementation.
+ *
+ * This adapter uses the `phpredis` PHP extension, which can be found here:
+ * http://github.com/owlient/phpredis
+ *
+ * The Redis cache adapter is meant to be used through the `Cache` interface,
+ * which abstracts away key generation, adapter instantiation and filter
+ * implementation. This adapter does not aim to provide a full implementation of the
+ * Redis API, but rather only a subset of its features that are useful in the context of a
+ * semi-persistent cache.
+ *
+ * A simple configuration of this adapter can be accomplished in `app/config/bootstrap.php`
+ * as follows:
+ *
+ * {{{
+ * Cache::config(array(
+ *     'cache-config-name' => array(
+ *         'adapter' => 'Redis',
+ *         'server' => '127.0.0.1:6379'
+ *     )
+ * ));
+ * }}}
+ *
+ * The 'server' key accepts a string argument in the format of ipaddress:port where the Redis
+ * server can be found.
+ *
+ * This Redis adapter provides basic support for `write`, `read`, `delete`
+ * and `clear` cache functionality, as well as allowing the first four
+ * methods to be filtered as per the Lithium filtering system. Additionally,
+ * This adapter defines several methods that are _not_ implemented in other
+ * adapters, and are thus non-portable - see the documentation for `Cache`
+ * as to how these methods should be accessed.
+ *
+ * @see lithium\storage\Cache::key()
+ * @see lithium\storage\Cache::adapter()
+ *
+ */
+class Redis extends \lithium\core\Object {
+
+	/**
+	 * Redis object instance used by this adapter.
+	 *
+	 * @var object Memcache object
+	 */
+	protected static $_Redis = null;
+
+	/**
+	 * Object constructor
+	 *
+	 * Instantiates the Redis object and connects it to the configured server.
+	 *
+	 * @param array $config Configuration parameters for this cache adapter.
+	 *        These settings are indexed by name and queryable through `Cache::config('name')`.
+	 *
+	 * @return void
+	 * @see lithium\storage\Cache::config()
+	 * @todo Implement configurable & optional authentication
+	 */
+	public function __construct($config = array()) {
+		$defaults = array(
+			'prefix' => '',
+			'server' => '127.0.0.1:6379'
+		);
+
+		if (is_null(static::$_Redis)) {
+			static::$_Redis = new \Redis();
+		}
+
+		$config += $defaults;
+		parent::__construct($config);
+
+		list($IP, $port) = explode(':', $this->_config['server']);
+		static::$_Redis->connect($IP, $port);
+	}
+
+	/**
+	 * Sets expiration time for cache keys
+	 *
+	 * @param string $key The key to uniquely identify the cached item
+	 * @param string $expiry A strtotime() compatible cache time
+	 * @return boolean True if expiry could be set for the given key, false otherwise
+	 */
+	protected function _ttl($key, $expiry) {
+		$expires = strtotime($expiry) - time();
+		return static::$_Redis->setTimeout($key, $expires);
+	}
+
+	/**
+	 * Write value(s) to the cache
+	 *
+	 * @param string $key The key to uniquely identify the cached item
+	 * @param mixed $value The value to be cached
+	 * @param string $expiry A strtotime() compatible cache time
+	 * @return boolean True on successful write, false otherwise
+	 */
+	public function write($key, $value, $expiry) {
+		$Redis =& static::$_Redis;
+
+		return function($self, $params, $chain) use (&$Redis) {
+			extract($params);
+			if($Redis->set($key, $data)){
+				return $self->invokeMethod('_ttl', array($key, $expiry));
+			}
+		};
+	}
+
+	/**
+	 * Read value(s) from the cache
+	 *
+	 * @param string $key The key to uniquely identify the cached item
+	 * @return mixed Cached value if successful, false otherwise
+	 */
+	public function read($key) {
+		$Redis =& static::$_Redis;
+
+		return function($self, $params, $chain) use (&$Redis) {
+			extract($params);
+			return $Redis->get($key);
+		};
+	}
+
+	/**
+	 * Delete value from the cache
+	 *
+	 * @param string $key        The key to uniquely identify the cached item
+	 * @return mixed True on successful delete, false otherwise
+	 */
+	public function delete($key) {
+		$Redis =& static::$_Redis;
+
+		return function($self, $params, $chain) use (&$Redis) {
+			extract($params);
+			return (boolean)$Redis->delete($key);
+		};
+	}
+
+	/**
+	 * Performs an atomic decrement operation on specified numeric cache item.
+	 *
+	 * @param string $key Key of numeric cache item to decrement
+	 * @param integer $offset Offset to decrement - defaults to 1.
+	 * @return mixed Item's new value on successful decrement, false otherwise
+	 */
+	public function decrement($key, $offset = 1) {
+		$Redis =& static::$_Redis;
+
+		return function($self, $params, $chain) use (&$Redis, $offset) {
+			extract($params);
+			return $Redis->decr($key, $offset);
+		};
+	}
+
+	/**
+	 * Performs an atomic increment operation on specified numeric cache item.
+	 *
+	 * @param string $key Key of numeric cache item to increment
+	 * @param integer $offset Offset to increment - defaults to 1.
+	 * @return mixed Item's new value on successful increment, false otherwise
+	 */
+	public function increment($key, $offset = 1) {
+		$Redis =& static::$_Redis;
+
+		return function($self, $params, $chain) use (&$Redis, $offset) {
+			extract($params);
+			return $Redis->incr($key, $offset);
+		};
+	}
+
+	/**
+	 * Clears user-space cache
+	 *
+	 * @return mixed True on successful clear, false otherwise
+	 */
+	public function clear() {
+		return static::$_Redis->flushdb();
+	}
+
+	/**
+	 * Determines if the Redis extension has been installed and
+	 * that there is a redis-server available
+	 *
+	 * return boolean True if enabled, false otherwise
+	 */
+	public function enabled() {
+		if (!extension_loaded('redis')) {
+			return false;
+		}
+		$version = static::$_Redis->info();
+		return (!empty($version));
+	}
+}
+
+?>
---
+++ libraries/lithium/tests/cases/storage/cache/adapter/RedisTest.php
@@ -0,0 +1,326 @@
+<?php
+/**
+ * Lithium: the most rad php framework
+ *
+ * @copyright     Copyright 2009, Union of RAD (http://union-of-rad.org)
+ * @license       http://opensource.org/licenses/bsd-license.php The BSD License
+ */
+
+namespace lithium\tests\cases\storage\cache\adapter;
+
+use \lithium\storage\cache\adapter\Redis;
+
+class RedisTest extends \lithium\test\Unit {
+
+	/**
+	 * Skip the test if the Redis extension is unavailable.
+	 *
+	 * @return void
+	 */
+	public function skip() {
+		$extensionExists = extension_loaded('redis');
+		$message = 'The redis extension is not installed.';
+		$this->skipIf(!$extensionExists, $message);
+
+		$R = new \Redis();
+		$R->connect('127.0.0.1', 6379);
+		$message = 'redis-server does not appear to be running on 127.0.0.1:6379';
+		$result = $R->info();
+		$this->skipIf(empty($result), $message);
+		unset($R);
+	}
+
+	public function setUp() {
+		$this->server = array('host' => '127.0.0.1', 'port' => 6379);
+		$this->_Redis = new \Redis();
+		$this->_Redis->connect($this->server['host'], $this->server['port']);
+		$this->Redis = new Redis();
+	}
+
+	public function tearDown() {
+		$this->_Redis->flushdb();
+	}
+
+	public function testEnabled() {
+		$this->assertTrue($this->Redis->enabled());
+	}
+
+	public function testSimpleWrite() {
+		$key = 'key';
+		$data = 'value';
+		$expiry = '+5 seconds';
+		$time = strtotime($expiry);
+
+		$closure = $this->Redis->write($key, $data, $expiry);
+		$this->assertTrue(is_callable($closure));
+
+		$params = compact('key', 'data', 'expiry');
+		$result = $closure($this->Redis, $params, null);
+		$expected = $data;
+		$this->assertEqual($expected, $result);
+
+		$result = $this->_Redis->get($key);
+		$this->assertEqual($expected, $result);
+
+		$result = $this->_Redis->ttl($key);
+		$this->assertEqual($time - time(), $result);
+
+		$result = $this->_Redis->delete($key);
+		$this->assertTrue($result);
+
+		$key = 'another_key';
+		$data = 'more_data';
+		$expiry = '+1 minute';
+		$time = strtotime($expiry);
+
+		$closure = $this->Redis->write($key, $data, $expiry);
+		$this->assertTrue(is_callable($closure));
+
+		$params = compact('key', 'data', 'expiry');
+		$result = $closure($this->Redis, $params, null);
+		$expected = $data;
+		$this->assertEqual($expected, $result);
+
+		$result = $this->_Redis->get($key);
+		$this->assertEqual($expected, $result);
+
+		$result = $this->_Redis->ttl($key);
+		$this->assertEqual($time - time(), $result);
+
+		$result = $this->_Redis->delete($key);
+		$this->assertTrue($result);
+	}
+
+	public function testSimpleRead() {
+		$key = 'read_key';
+		$data = 'read data';
+		$time = strtotime('+1 minute');
+
+		$result = $this->_Redis->set($key, $data);
+		$this->assertTrue($result);
+
+		$closure = $this->Redis->read($key);
+		$this->assertTrue(is_callable($closure));
+
+		$params = compact('key');
+		$result = $closure($this->Redis, $params, null);
+		$expected = $data;
+		$this->assertEqual($expected, $result);
+
+		$result = $this->_Redis->delete($key);
+		$this->assertTrue($result);
+
+		$key = 'another_read_key';
+		$data = 'read data';
+		$time = strtotime('+1 minute');
+
+		$result = $this->_Redis->set($key, $data);
+		$this->assertTrue($result);
+
+		$result = $this->_Redis->ttl($key);
+		$this->assertTrue($result);
+
+		$closure = $this->Redis->read($key);
+		$this->assertTrue(is_callable($closure));
+
+		$params = compact('key');
+		$result = $closure($this->Redis, $params, null);
+		$expected = $data;
+		$this->assertEqual($expected, $result);
+
+		$result = $this->_Redis->delete($key);
+		$this->assertTrue($result);
+	}
+
+	public function testReadKeyThatDoesNotExist() {
+		$key = 'does_not_exist';
+		$closure = $this->Redis->read($key);
+		$this->assertTrue(is_callable($closure));
+
+		$params = compact('key');
+		$result = $closure($this->Redis, $params, null);
+		$this->assertFalse($result);
+
+	}
+
+	public function testDelete() {
+		$key = 'delete_key';
+		$data = 'data to delete';
+		$time = strtotime('+1 minute');
+
+		$result = $this->_Redis->set($key, $data);
+		$this->assertTrue($result);
+
+		$closure = $this->Redis->delete($key);
+		$this->assertTrue(is_callable($closure));
+
+		$params = compact('key');
+		$result = $closure($this->Redis, $params, null);
+		$this->assertTrue($result);
+
+		$this->assertFalse($this->_Redis->delete($key));
+	}
+
+	public function testDeleteNonExistentKey() {
+		$key = 'delete_key';
+		$closure = $this->Redis->delete($key);
+		$this->assertTrue(is_callable($closure));
+
+		$params = compact('key');
+		$result = $closure($this->Redis, $params, null);
+		$this->assertFalse($result);
+	}
+
+	public function testWriteReadAndDeleteRoundtrip() {
+		$key = 'write_read_key';
+		$data = 'write/read value';
+		$expiry = '+5 seconds';
+		$time = strtotime($expiry);
+
+		$closure = $this->Redis->write($key, $data, $expiry);
+		$this->assertTrue(is_callable($closure));
+
+		$params = compact('key', 'data', 'expiry');
+		$result = $closure($this->Redis, $params, null);
+		$expected = $data;
+		$this->assertEqual($expected, $result);
+
+		$result = $this->_Redis->get($key);
+		$this->assertEqual($expected, $result);
+
+		$closure = $this->Redis->read($key);
+		$this->assertTrue(is_callable($closure));
+
+		$params = compact('key');
+		$result = $closure($this->Redis, $params, null);
+		$expected = $data;
+		$this->assertEqual($expected, $result);
+
+		$closure = $this->Redis->delete($key);
+		$this->assertTrue(is_callable($closure));
+
+		$params = compact('key');
+		$result = $closure($this->Redis, $params, null);
+		$this->assertTrue($result);
+
+		$this->assertFalse($this->_Redis->get($key));
+	}
+
+	public function testExpiredRead() {
+		$key = 'expiring_read_key';
+		$data = 'expired data';
+		$time = 1;
+
+		$result = $this->_Redis->set($key, $data);
+		$this->assertTrue($result);
+
+		$result = $this->_Redis->setTimeout($key, $time);
+		$this->assertTrue($result);
+
+		sleep($time + 1);
+		$closure = $this->Redis->read($key);
+		$this->assertTrue(is_callable($closure));
+
+		$params = compact('key');
+		$result = $closure($this->Redis, $params, null);
+		$this->assertFalse($result);
+	}
+
+	public function testClear() {
+		$result = $this->_Redis->set('key', 'value');
+		$this->assertTrue($result);
+
+		$result = $this->_Redis->set('another_key', 'value');
+		$this->assertTrue($result);
+
+		$result = $this->Redis->clear();
+		$this->assertTrue($result);
+
+		$this->assertFalse($this->_Redis->get('key'));
+		$this->assertFalse($this->_Redis->get('another_key'));
+	}
+
+	public function testDecrement() {
+		$key = 'decrement';
+		$value = 10;
+
+		$result = $this->_Redis->set($key, $value);
+		$this->assertTrue($result);
+
+		$closure = $this->Redis->decrement($key);
+		$this->assertTrue(is_callable($closure));
+
+		$params = compact('key');
+		$result = $closure($this->Redis, $params, null);
+		$this->assertEqual($value - 1, $result);
+
+		$result = $this->_Redis->get($key);
+		$this->assertEqual($value - 1, $result);
+
+		$result = $this->_Redis->delete($key);
+		$this->assertTrue($result);
+	}
+
+	public function testDecrementNonIntegerValue() {
+		$key = 'non_integer';
+		$value = 'no';
+
+		$result = $this->_Redis->set($key, $value);
+		$this->assertTrue($result);
+
+		$closure = $this->Redis->decrement($key);
+		$this->assertTrue(is_callable($closure));
+
+		$params = compact('key');
+		$result = $closure($this->Redis, $params, null);
+
+		$result = $this->_Redis->get($key);
+		$this->assertEqual(-1, $result);
+
+		$result = $this->_Redis->delete($key);
+		$this->assertTrue($result);
+	}
+
+	public function testIncrement() {
+		$key = 'increment';
+		$value = 10;
+
+		$result = $this->_Redis->set($key, $value);
+		$this->assertTrue($result);
+
+		$closure = $this->Redis->increment($key);
+		$this->assertTrue(is_callable($closure));
+
+		$params = compact('key');
+		$result = $closure($this->Redis, $params, null);
+		$this->assertEqual($value + 1, $result);
+
+		$result = $this->_Redis->get($key);
+		$this->assertEqual($value + 1, $result);
+
+		$result = $this->_Redis->delete($key);
+		$this->assertTrue($result);
+	}
+
+	public function testIncrementNonIntegerValue() {
+		$key = 'non_integer_increment';
+		$value = 'yes';
+
+		$result = $this->_Redis->set($key, $value);
+		$this->assertTrue($result);
+
+		$closure = $this->Redis->increment($key);
+		$this->assertTrue(is_callable($closure));
+
+		$params = compact('key');
+		$result = $closure($this->Redis, $params, null);
+
+		$result = $this->_Redis->get($key);
+		$this->assertEqual(1, $result);
+
+		$result = $this->_Redis->delete($key);
+		$this->assertTrue($result);
+	}
+}
+
+?>
