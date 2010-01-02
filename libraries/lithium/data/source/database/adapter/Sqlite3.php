2b13810b56a47709e91251f0ee41e201868a731c
Niel Archer <niel.archer@blueyonder.co.uk>
2010-01-02 17:42:40 +0000

    Adding @params to doc blocks for __construct() method.

--- libraries/lithium/data/source/database/adapter/Sqlite3.php
+++ libraries/lithium/data/source/database/adapter/Sqlite3.php
@@ -42,11 +42,11 @@ class Sqlite3 extends \lithium\data\source\Database {
 	 * @param array $config Configuration options for this class. For additional configuration,
 	 *        see `lithium\data\source\Database` and `lithium\data\Source`. Available options
 	 *        defined by this class:
-	 *        - 'database' _string_: database name. Defaults to none
-	 *        - 'flags' _integer_: Optional flags used to determine how to open the SQLite database.
-	 *          By default, open uses SQLITE3_OPEN_READWRITE | SQLITE3_OPEN_CREATE.
-	 *        - 'key' _string_: An optional encryption key used when encrypting and
-	 *          decrypting an SQLite database.
+	 *        - `'database'` _string_: database name. Defaults to none
+	 *        - `'flags'` _integer_: Optional flags used to determine how to open the SQLite
+	 *          database. By default, open uses SQLITE3_OPEN_READWRITE | SQLITE3_OPEN_CREATE.
+	 *        - `'key'` _string_: An optional encryption key used when encrypting and decrypting
+	 *          an SQLite database.
 	 *
 	 * Typically, these parameters are set in `Connections::add()`, when adding the adapter to the
 	 * list of active connections.
