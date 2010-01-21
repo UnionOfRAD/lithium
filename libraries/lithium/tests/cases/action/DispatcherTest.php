ecdb060a1691c6655988906e9ed1625cc8a20978
Nate Abele <nate.abele@gmail.com>
2010-01-21 00:01:34 -0500

    Adding assertions and mock class for `action\Dispatcher`.

--- libraries/lithium/tests/cases/action/DispatcherTest.php
+++ libraries/lithium/tests/cases/action/DispatcherTest.php
@@ -8,9 +8,38 @@
 
 namespace lithium\tests\cases\action;
 
+use \lithium\http\Router;
+use \lithium\http\Request;
 use \lithium\action\Dispatcher;
+use \lithium\tests\mocks\action\MockDispatcher;
 
 class DispatcherTest extends \lithium\test\Unit {
+
+	protected $_routes = array();
+
+	public function setUp() {
+		$this->_routes = Router::get();
+		Router::connect(null);
+	}
+
+	public function tearDown() {
+		Router::connect(null);
+
+		foreach ($this->_routes as $route) {
+			Router::connect($route);
+		}
+	}
+
+	public function testRun() {
+		Router::connect('/', array('controller' => 'test', 'action' => 'test'));
+		$request = new Request();
+		$request->url = '/';
+		MockDispatcher::run($request);
+
+		$result = end(MockDispatcher::$dispatched);
+		$expected = array('controller' => 'test', 'action' => 'test');
+		$this->assertEqual($expected, $result->params);
+	}
 }
 
 ?>
---
+++ libraries/lithium/tests/mocks/action/MockDispatcher.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Lithium: the most rad php framework
+ *
+ * @copyright     Copyright 2009, Union of RAD (http://union-of-rad.org)
+ * @license       http://opensource.org/licenses/bsd-license.php The BSD License
+ */
+
+namespace lithium\tests\mocks\action;
+
+use \stdClass;
+
+class MockDispatcher extends \lithium\action\Dispatcher {
+
+	public static $dispatched = array();
+
+	protected static function _callable($request, $params, $options) {
+		$callable = new stdClass();
+		$callable->params = $params;
+		return $callable;
+	}
+
+	protected static function _call($callable, $request, $params) {
+		static::$dispatched[] = $callable;
+	}
+}
+
+?>
