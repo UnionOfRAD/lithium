3167c0f2b618e903b13eb4fcb1be017c0f60a864
psychic <psychic@cakephp.org>
2010-01-29 14:27:13 -0700

    Adding style names to color listings.

--- app/config/bootstrap/console.php
+++ app/config/bootstrap/console.php
@@ -11,26 +11,30 @@ use \lithium\util\String;
 
 ConsoleDispatcher::applyFilter('_call', function($self, $params, $chain) {
 	$params['callable']->response->applyFilter('output', function($self, $params, $chain) {	
+		$styles = array(
+			/** General **/
+			'heading1' => "\033[1;36m",
+			'heading2' => "\033[1;34m",
+			'heading3' => "\033[1;35m",
+			'option'   => "\033[1;33m",
+			'command'  => "\033[1;32m",
+			/** Colors **/
+			'black'  => "\033[0;30m",
+			'red'    => "\033[0;31m",
+			'green'  => "\033[0;32m",
+			'yellow' => "\033[0;33m",
+			'blue'   => "\033[0;34m",
+			'purple' => "\033[0;35m",
+			'cyan'   => "\033[0;36m",
+			'white'  => "\033[0;37m",
+			'end'    => "\033[0m",
+		);
 		if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-		    $colors = array(
-				'black' => '', 'red' => '', 'green' => '', 'yellow' => '',
-				'blue' => '', 'purple' => '', 'cyan' => '', 'white' => '', 
-				'end' => ''
-			);
-		} else {
-			$colors = array(
-				'black'  => "\033[0;30m",
-				'red'    => "\033[0;31m",
-				'green'  => "\033[0;32m",
-				'yellow' => "\033[0;33m",
-				'blue'   => "\033[0;34m",
-				'purple' => "\033[0;35m",
-				'cyan'   => "\033[0;36m",
-				'white'  => "\033[0;37m",
-				'end'    => "\033[0m",
-			);
+			array_walk($styles, function(&$value, $key) {
+				$value = '';
+			});
 		}
-		$params['string'] = String::insert($params['string'], $colors);
+		$params['string'] = String::insert($params['string'], $styles);
 		return $chain->next($self, $params, $chain);
 	}); 
 	return $chain->next($self, $params, $chain);
