94f8fdcfd4af0c9d4d11a2c9d137208c2e8c74bf
gwoo <gwoohoo@gmail.com>
2010-02-10 09:34:17 -0800

    adding realpath fix to \test\GroupTest /thanks @coates

--- libraries/lithium/tests/cases/test/GroupTest.php
+++ libraries/lithium/tests/cases/test/GroupTest.php
@@ -122,8 +122,8 @@ class GroupTest extends \lithium\test\Unit {
 		$result = $results[0][0]['class'];
 		$this->assertEqual($expected, $result);
 
-		$expected = str_replace('\\', '/', LITHIUM_LIBRARY_PATH)
-			. '/lithium/tests/mocks/test/MockUnitTest.php';
+		$expected = realpath(str_replace('\\', '/', LITHIUM_LIBRARY_PATH)
+			. '/lithium/tests/mocks/test/MockUnitTest.php');
 		$result = $results[0][0]['file'];
 		$this->assertEqual($expected, str_replace('\\', '/', $result));
 	}
