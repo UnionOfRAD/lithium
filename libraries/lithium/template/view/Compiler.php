1b937fe259a8330f3bd7b5eb9165f68aa01363ca
Nate Abele <nate.abele@gmail.com>
2010-02-11 18:02:33 -0500

    Minor refactorings to template compilation per code review by Sean Coates.

--- libraries/lithium/template/view/Compiler.php
+++ libraries/lithium/template/view/Compiler.php
@@ -19,8 +19,8 @@ class Compiler extends \lithium\core\StaticObject {
 	 * The list of syntax replacements to apply to compiled templates.
 	 */
 	protected static $_processors = array(
-		'/\<\?=\s*\$this->(.+?)\s*;?\s*\?>/ms' => '<?php echo $this->$1; ?>',
-		'/\<\?=\s*(.+?)\s*;?\s*\?>/ms' => '<?php echo $h($1); ?>'
+		'/\<\?=\s*\$this->(.+?)\s*;?\s*\?>/msx' => '<?php echo $this->$1; ?>',
+		'/\<\?=\s*(.+?)\s*;?\s*\?>/msx' => '<?php echo $h($1); ?>'
 	);
 
 	/**
@@ -34,10 +34,10 @@ class Compiler extends \lithium\core\StaticObject {
 		$defaults = array('path' => LITHIUM_APP_PATH . '/resources/tmp/cache/templates');
 		$options += $defaults;
 
-		$oname = basename($file);
 		$stats = stat($file);
-		extract($stats);
-		$template = "{$options['path']}/template_{$oname}_{$ino}_{$mtime}_{$size}.php";
+		$oname = basename($file, '.php');
+		$template = "template_{$oname}_{$stats['ino']}_{$stats['mtime']}_{$stats['size']}.php";
+		$template = "{$options['path']}/{$template}";
 
 		if (!file_exists($template)) {
 			file_put_contents($template, static::compile(file_get_contents($file)));
@@ -46,10 +46,8 @@ class Compiler extends \lithium\core\StaticObject {
 	}
 
 	public static function compile($string) {
-		foreach (static::$_processors as $pattern => $replacement) {
-			$string = preg_replace($pattern, $replacement, $string);
-		}
-		return $string;
+		$patterns = static::$_processors;
+		return preg_replace(array_keys($patterns), array_values($patterns), $string);
 	}
 }
 
