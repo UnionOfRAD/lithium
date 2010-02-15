a832eb1029f82045f25f7b68fbf9e092db0a3074
Nate Abele <nate.abele@gmail.com>
2010-02-15 17:32:17 -0500

    Uncommenting console bootstrap configuration, since it should execute automatically if included.

--- app/config/bootstrap/console.php
+++ app/config/bootstrap/console.php
@@ -6,14 +6,14 @@
  * @license       http://opensource.org/licenses/bsd-license.php The BSD License
  */
 
-// use \lithium\console\Dispatcher;
-//
-// Dispatcher::applyFilter('_call', function($self, $params, $chain) {
-// 	$params['callable']->response->styles(array(
-// 		'heading' => '\033[1;30;46m'
-// 	));
-// 	return $chain->next($self, $params, $chain);
-// });
+use lithium\console\Dispatcher;
+
+Dispatcher::applyFilter('_call', function($self, $params, $chain) {
+	$params['callable']->response->styles(array(
+		'heading' => '\033[1;30;46m'
+	));
+	return $chain->next($self, $params, $chain);
+});
 
 
 ?>
