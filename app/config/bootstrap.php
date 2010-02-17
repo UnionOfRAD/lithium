5e551a33cd8d5900e7ffc19371f376eb5c16472a
Nate Abele <nate.abele@gmail.com>
2010-02-17 12:03:49 -0500

    Adding `config/bootstrap/media.php` to contain rules for handling different media types, and serving static assets.

--- app/config/bootstrap.php
+++ app/config/bootstrap.php
@@ -54,6 +54,12 @@ require __DIR__ . '/bootstrap/libraries.php';
 // require __DIR__ . '/bootstrap/g11n.php';
 
 /**
+ * This file contains configurations for handling different content types within the framework,
+ * including converting data to and from different formats, and handling static media assets.
+ */
+// require __DIR__ . '/bootstrap/media.php';
+
+/**
  * This file configures console filters and settings, specifically output behavior and coloring.
  */
 // require __DIR__ . '/bootstrap/console.php';
@@ -68,25 +74,5 @@ require __DIR__ . '/bootstrap/libraries.php';
 // Session::config(array(
 // 	'default' => array('adapter' => 'Php')
 // ));
-
-/**
- * The `Collection` class, which serves as the base class for some of Lithium's data objects
- * (`RecordSet` and `Document`) provides a way to manage data collections in a very flexible and
- * intuitive way, using closures and SPL interfaces. The `to()` method allows a `Collection` (or
- * subclass) to be converted to another format, such as an array. The `Collection` class also allows
- * other classes to be connected as handlers to convert `Collection` objects to other formats.
- *
- * The following connects the `Media` class as a format handler, which allows `Collection`s to be
- * exported to any format with a handler provided by `Media`, i.e. JSON. This enables things like
- * the following:
- * {{{
- * $posts = Post::find('all');
- * return $posts->to('json');
- * }}}
- */
-// use \lithium\util\Collection;
-//
-// Collection::formats('\lithium\net\http\Media');
-
 
 ?>
