093d591441a5d0a7ca2f27e7d6ed1003ba05d3ae
David Persson <davidpersson@gmx.de>
2010-02-20 14:05:30 +0100

    Adding tests for `Php` catalog adapter.

--- libraries/lithium/tests/cases/g11n/catalog/adapter/PhpTest.php
+++ libraries/lithium/tests/cases/g11n/catalog/adapter/PhpTest.php
@@ -8,11 +8,14 @@
 
 namespace lithium\tests\cases\g11n\catalog\adapter;
 
+use \Exception;
 use \lithium\g11n\catalog\adapter\Php;
 
 class PhpTest extends \lithium\test\Unit {
 
 	public $adapter;
+
+	protected $_path;
 
 	public function skip() {
 		$this->_path = $path = LITHIUM_APP_PATH . '/resources/tmp/tests';
@@ -21,7 +24,6 @@ class PhpTest extends \lithium\test\Unit {
 	}
 
 	public function setUp() {
-		mkdir($this->_path . '/fr/message', 0755, true);
 		$this->adapter = new Php(array('path' => $this->_path));
 	}
 
@@ -29,7 +31,27 @@ class PhpTest extends \lithium\test\Unit {
 		$this->_cleanUp();
 	}
 
+	public function testPathMustExist() {
+		try {
+			new Php(array('path' => $this->_path));
+			$result = true;
+		} catch (Exception $e) {
+			$result = false;
+		}
+		$this->assert($result);
+
+		try {
+			new Php(array('path' => "{$this->_path}/i_do_not_exist"));
+			$result = false;
+		} catch (Exception $e) {
+			$result = true;
+		}
+		$this->assert($result);
+	}
+
 	public function testRead() {
+		mkdir("{$this->_path}/fr/message", 0755, true);
+
 		$data = array(
 			'politics' => 'politique',
 			'house' => array('maison', 'maisons')
@@ -37,7 +59,7 @@ class PhpTest extends \lithium\test\Unit {
 		$contents[] = '<?php';
 		$contents[] = 'return ' . var_export($data, true);
 		$contents[] = '?>';
-		file_put_contents($this->_path . '/fr/message/default.php', implode("\n", $contents));
+		file_put_contents("{$this->_path}/fr/message/default.php", implode("\n", $contents));
 
 		$result = $this->adapter->read('message', 'fr', null);
 		$expected = array(
@@ -58,6 +80,65 @@ class PhpTest extends \lithium\test\Unit {
 				'occurrences' => array()
 		));
 		$this->assertEqual($expected, $result);
+	}
+
+	public function testReadTemplate() {
+		$data = array(
+			'politics' => 'politique',
+			'house' => array('maison', 'maisons')
+		);
+		$contents[] = '<?php';
+		$contents[] = 'return ' . var_export($data, true);
+		$contents[] = '?>';
+		file_put_contents("{$this->_path}/message_default.php", implode("\n", $contents));
+
+		$result = $this->adapter->read('messageTemplate', 'root', null);
+		$expected = array(
+			'politics' => array(
+				'id' => 'politics',
+				'ids' => array(),
+				'translated' => 'politique',
+				'flags' => array(),
+				'comments' => array(),
+				'occurrences' => array()
+			),
+			'house' => array(
+				'id' => 'house',
+				'ids' => array(),
+				'translated' => array('maison', 'maisons'),
+				'flags' => array(),
+				'comments' => array(),
+				'occurrences' => array()
+		));
+		$this->assertEqual($expected, $result);
+	}
+
+	public function testReadWithScope() {
+		mkdir("{$this->_path}/fr/message", 0755, true);
+
+		$data = array(
+			'politics' => 'politique'
+		);
+		$contents[] = '<?php';
+		$contents[] = 'return ' . var_export($data, true);
+		$contents[] = '?>';
+		file_put_contents("{$this->_path}/fr/message/li3_docs.php", implode("\n", $contents));
+
+		$result = $this->adapter->read('message', 'fr', null);
+		$this->assertFalse($result);
+
+		$result = $this->adapter->read('message', 'fr', 'li3_docs');
+		$expected = array(
+			'politics' => array(
+				'id' => 'politics',
+				'ids' => array(),
+				'translated' => 'politique',
+				'flags' => array(),
+				'comments' => array(),
+				'occurrences' => array()
+		));
+		$this->assertEqual($expected, $result);
+
 	}
 }
 
