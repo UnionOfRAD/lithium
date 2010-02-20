be7acfb4d693a2fda862aba713b9b6d253f02bc3
indiefan <indiefan@gmail.com>
2010-02-20 00:18:26 -0800

    Fixed Coverage filter template bug where empty data was being displayed

--- libraries/lithium/test/templates/coverage.html.php
+++ libraries/lithium/test/templates/coverage.html.php
@@ -5,16 +5,18 @@
 		lines covered (<em><?= $coverage['percentage'] ?>%</em>)
 	</h4>
 	<?php foreach ($coverage['output'] as $file => $data): ?>
-		<div class="code-coverage-results">
-			<h4 class="name"><?= $file ?></h4>
-			<?php foreach ($data as $line => $row): ?>
-				<div class="code-line <?= $row['class'] ?>">
-					<span class="line-num"><?= $line ?></span>
-					<span class="content"><?=
-						htmlspecialchars(str_replace("\t", "	", $row['data']))
-					?></span>
-				</div><!-- code-line -->
-			<?php endforeach ?>
-		</div><!-- code-coverage-results -->
+		<?php if (!empty($data)): ?>
+			<div class="code-coverage-results">
+				<h4 class="name"><?= $file ?></h4>
+				<?php foreach ($data as $line => $row): ?>
+					<div class="code-line <?= $row['class'] ?>">
+						<span class="line-num"><?= $line ?></span>
+						<span class="content"><?=
+							htmlspecialchars(str_replace("\t", "	", $row['data']))
+						?></span>
+					</div><!-- code-line -->
+				<?php endforeach ?>
+			</div><!-- code-coverage-results -->
+		<?php endif ?>
 	<?php endforeach ?>
 <?php endforeach ?>
