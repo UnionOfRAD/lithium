a914b3134a828edb504b00fbfa71e2a1f626f460
indiefan <indiefan@gmail.com>
2010-02-23 19:07:18 -0800

    Fixed stats template output.

--- libraries/lithium/test/templates/stats.html.php
+++ libraries/lithium/test/templates/stats.html.php
@@ -1,24 +1,24 @@
-<div class="test-result test-result-<?php ($success ? 'success' : 'fail') ?>">
-<?php $count['passes'] ?> / <?php $count['asserts'] ?> passes, <?php $count['fails'] ?>
-<?php ((intval($count['fails']) == 1) ? ' fail' : ' fails') ?> and <?php $count['exceptions'] ?>
-<?php ((intval($count['exceptions']) == 1) ? ' exception' : ' exceptions') ?>
+<div class="test-result test-result-<?php echo ($success ? 'success' : 'fail') ?>">
+<?php echo $count['passes'] ?> / <?php echo $count['asserts'] ?> passes, <?php echo $count['fails'] ?>
+<?php echo ((intval($count['fails']) == 1) ? ' fail' : ' fails') ?> and <?php echo $count['exceptions'] ?>
+<?php echo ((intval($count['exceptions']) == 1) ? ' exception' : ' exceptions') ?>
 </div>
 
 <?php foreach ((array) $stats['errors'] as $error): ?>
 	<?php if ($error['result'] == 'fail'): ?>
 		<div class="test-assert test-assert-failed">
-			Assertion '<?php $error['assertion'] ?>' failed in
-			<?php $error['class'] ?>::<?php $error['method']?>() on line
-			<?php $error['line'] ?>:
-			<span class="content"><?php $error['message'] ?></span>
+			Assertion '<?php echo $error['assertion'] ?>' failed in
+			<?php echo $error['class'] ?>::<?php echo $error['method']?>() on line
+			<?php echo $error['line'] ?>:
+			<span class="content"><?php echo $error['message'] ?></span>
 		</div>
 	<?php elseif ($error['result'] == 'exception'): ?>
 		<div class="test-exception">
-			Exception thrown in <?php $error['class'] ?>::<?php $error['method'] ?>()
-			on line <?php $error['line'] ?>:
-			<span class="content"><?php $error['message'] ?></span>
+			Exception thrown in <?php echo $error['class'] ?>::<?php echo $error['method'] ?>()
+			on line <?php echo $error['line'] ?>:
+			<span class="content"><?php echo $error['message'] ?></span>
 			<?php if (isset($error['trace']) && !empty($error['trace'])): ?>
-				Trace: <span class="trace"><?php $error['trace'] ?></span>
+				Trace: <span class="trace"><?php echo $error['trace'] ?></span>
 			<?php endif ?>
 		</div>
 	<?php endif ?>
@@ -26,8 +26,8 @@
 
 <?php foreach ((array) $stats['skips'] as $skip): ?>
 	<div class="test-skip">
-		Skip <?php $skip['trace'][1]['class'] ?>::<?php $skip['trace'][1]['function'] ?>()
-		on line <?php $skip['trace'][1]['line'] ?>:
-		<span class="content"><?php $skip['message'] ?></span>
+		Skip <?php echo $skip['trace'][1]['class'] ?>::<?php echo $skip['trace'][1]['function'] ?>()
+		on line <?php echo $skip['trace'][1]['line'] ?>:
+		<span class="content"><?php echo $skip['message'] ?></span>
 	</div>
 <?php endforeach ?>
