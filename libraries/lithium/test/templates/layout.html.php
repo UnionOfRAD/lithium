ec471c2fb3064af288f23eb174426bb35e6de4de
Nate Abele <nate.abele@gmail.com>
2010-02-28 15:59:57 -0500

    Fixing whitespace formatting and line length in test suite layout template.

--- libraries/lithium/test/templates/layout.html.php
+++ libraries/lithium/test/templates/layout.html.php
@@ -7,56 +7,58 @@
 	<head>
 		<title></title>
 		<link rel="stylesheet" href="<?php echo $base;?>/css/debug.css" />
-		<link href="<?php echo $base;?>/favicon.ico" title="Icon" type="image/x-icon" rel="icon" />
-		<link href="<?php echo $base;?>/favicon.ico" title="Icon" type="image/x-icon" rel="shortcut icon" />
+		<link href="<?php echo $base; ?>/favicon.ico" type="image/x-icon" rel="icon" />
+		<link href="<?php echo $base; ?>/favicon.ico" type="image/x-icon" rel="shortcut icon" />
 	</head>
 	<body class="test-dashboard">
 		<div id="header">
 			<header>
-				<h1><a href="<?php echo $base ?>/test/"><span class="triangle"></span> Lithium Unit Test Dashboard</a></h1>
+				<h1>
+					<a href="<?php echo $base ?>/test/">
+						<span class="triangle"></span> Lithium Unit Test Dashboard
+					</a>
+				</h1>
 				<a class="test-all" href="<?php echo $base ?>/test/all">run all tests</a>
 			</header>
 		</div>
 
 		<div class="article">
-				<article>
-					<div class="test-menu">
+			<article>
+				<div class="test-menu">
+					<?php echo $report->render("menu", array("menu" => $menu, "base" => $base)) ?>
+				</div>
 
-						<?php echo $report->render("menu", array("menu" => $menu, "base" => $base)) ?>
-					</div>
+				<div class="test-content">
+					<?php if (is_array($report->title)) {
+						$report->title = join (', ', $report->title);
+					} ?>
+					<h2><span>test results for </span><?php echo $report->title; ?></h2>
+					<span class="filters">
+						<?php echo join('', array_map(
+							function($class) use ($request) {
+								$url = "?filters[]={$class}";
+								$name = join('', array_slice(explode("\\", $class), -1));
+								$key = Inflector::underscore($name);
+								$isActive = (
+									isset($request->query['filters']) &&
+									array_search($class, $request->query['filters']) !== false
+								);
+								$active = $isActive ? 'active' : null;
+								return "<a class=\"{$key} {$active}\" href=\"{$url}\">{$name}</a>";
+							},
+							$filters
+						)); ?>
+					</span>
+					<?php
 
-					<div class="test-content">
-						<?php if (is_array($report->title)) {
-							$report->title = join (', ', $report->title);
-						} ?>
-						<h2><span>test results for </span><?php echo $report->title; ?></h2>
-						<span class="filters">
-							<?php echo join('', array_map(
-								function($class) use ($request) {
-									$url = "?filters[]={$class}";
-									$name = join('', array_slice(explode("\\", $class), -1));
-									$key = Inflector::underscore($name);
-									$active = (
-										isset($request->query['filters']) &&
-										array_search($class, $request->query['filters']) !== false
-									) ?
-										'active' : null ;
-									return "<a class=\"{$key} {$active}\" href=\"{$url}\">{$name}</a>";
-								},
-								$filters
-							)); ?>
-						</span>
-						<?php echo $report->render("stats", $report->stats()) ?>
-						<?php foreach ($report->results['filters'] as $filter => $data): ?>
-							<?php
-								$filterClass = explode("\\", $filter);
-								$filterClass = array_pop($filterClass);
-								echo $report->render(
-									strtolower($filterClass),
-									array("analysis" => $data)
-								); ?>
-						<?php endforeach ?>
-					</div>
+					echo $report->render("stats", $report->stats());
+					foreach ($report->results['filters'] as $filter => $data) {
+						$filterClass = explode("\\", $filter);
+						$filterClass = array_pop($filterClass);
+						echo $report->render(strtolower($filterClass), array("analysis" => $data));
+					}
+					?>
+				</div>
 			</article>
 		</div>
 		<div style="clear:both"></div>
