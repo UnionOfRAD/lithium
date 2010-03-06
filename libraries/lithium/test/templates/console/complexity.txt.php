b67d9577933a3dfba67ce25013e397d326dd064f
indiefan <indiefan@gmail.com>
2010-03-05 21:36:52 -0800

    Added initial Console templates for Test command and updated command to render them.

---
+++ libraries/lithium/test/templates/console/complexity.txt.php
@@ -0,0 +1,18 @@
+<?php
+echo "Cyclomatic Complexity:\n\n";
+
+foreach (array_slice($analysis['max'], 0, 10) as $method => $count) {
+	if ($count <= 7) {
+		continue;
+	}
+	echo "Worst Offender\n\t{$method} - {$count}\n";
+}
+echo "Class Averages\n";
+foreach (array_slice($analysis['class'], 0, 10) as $class => $count) {
+	echo "\t{$class} - ";
+	echo round($count, 2);
+}
+
+echo "\n";
+
+?>
