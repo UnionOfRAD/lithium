5d61132a62222b7d4835d050eb1b539de4768aeb
Nate Abele <nate.abele@gmail.com>
2010-04-19 09:33:17 -0400

    Adding mock model base class.

---
+++ libraries/lithium/tests/mocks/data/MockBase.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Lithium: the most rad php framework
+ *
+ * @copyright     Copyright 2010, Union of RAD(http://union-of-rad.org)
+ * @license       http://opensource.org/licenses/bsd-license.php The BSD License
+ */
+
+namespace lithium\tests\mocks\data;
+
+class MockBase extends \lithium\data\Model {
+
+	protected $_meta = array('connection' => 'mock-source');
+
+	public static function __init() {
+		static::_isBase(__CLASS__, true);
+	}
+}
+
+?>
