137137a24c49c558ba75ed327adc96900f15e329
Svemir Brkic <svemir@deveblog.com>
2011-01-13 20:40:35 -0500

    Use PHP_EOL instead of \n when splitting lines, for better cross-platform compatibility

--- libraries/lithium/analysis/Inspector.php
+++ libraries/lithium/analysis/Inspector.php
@@ -332,16 +332,16 @@ class Inspector extends \lithium\core\StaticObject {
 	 * @todo Add an $options parameter with a 'context' flag, to pull in n lines of context.
 	 */
 	public static function lines($data, $lines) {
-		if (!strpos($data, "\n")) {
+		if (!strpos($data, PHP_EOL)) {
 			if (!file_exists($data)) {
 				$data = Libraries::path($data);
 				if (!file_exists($data)) {
 					return null;
 				}
 			}
-			$data = "\n" . file_get_contents($data);
+			$data = PHP_EOL . file_get_contents($data);
 		}
-		$c = explode("\n", $data);
+		$c = explode(PHP_EOL, $data);
 
 		if (!count($c) || !count($lines)) {
 			return null;
