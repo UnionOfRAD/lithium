0a74653b067d1ef5a1b55874cb86b8a9356c01a0
gwoo <gwoohoo@gmail.com>
2011-02-07 04:49:38 -0800

    allowing \test\Controller timeout to be set via params.

--- libraries/lithium/test/Controller.php
+++ libraries/lithium/test/Controller.php
@@ -30,22 +30,22 @@ class Controller extends \lithium\core\Object {
 	public function __invoke($request, $dispatchParams, array $options = array()) {
 		$dispatchParamsDefaults = array('args' => array());
 		$dispatchParams += $dispatchParamsDefaults;
-		$defaults = array('reporter' => 'html', 'format' => 'html');
+		$defaults = array('reporter' => 'html', 'format' => 'html', 'timeout' => 0);
 		$options += (array) $request->query + $defaults;
 		$params = compact('request', 'dispatchParams', 'options');
-		set_time_limit(0);
 
 		return $this->_filter(__METHOD__, $params, function($self, $params) {
 			$request = $params['request'];
 			$options = $params['options'];
 			$params = $params['dispatchParams'];
+			set_time_limit((integer) $options['timeout']);
 			$group = join('\\', (array) $params['args']);
 
 			if ($group === "all") {
 				$group = Group::all();
 				$options['title'] = 'All Tests';
 			}
-			$report = Dispatcher::run($group , $options);
+			$report = Dispatcher::run($group, $options);
 			$filters = Libraries::locate('test.filter');
 			$menu = Libraries::locate('tests', null, array(
 				'filter' => '/cases|integration|functional/',
