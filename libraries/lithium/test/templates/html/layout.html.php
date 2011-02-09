2731f7c81bdb251b0e34716ee35a6da7413133c6
David Persson <davidpersson@gmx.de>
2011-02-10 00:49:18 +0100

    Splitting up/formatting code in the header section of test layout.

--- libraries/lithium/test/templates/html/layout.html.php
+++ libraries/lithium/test/templates/html/layout.html.php
@@ -1,6 +1,13 @@
 <?php
-	use lithium\util\Inflector;
-	$base = $request->env('base');
+/**
+ * Lithium: the most rad php framework
+ *
+ * @copyright     Copyright 2010, Union of RAD (http://union-of-rad.org)
+ * @license       http://opensource.org/licenses/bsd-license.php The BSD License
+ */
+
+use lithium\util\Inflector;
+
 ?>
 <!doctype html>
 <html>
