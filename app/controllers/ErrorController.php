b638990dcf90902042e438dc4372888dd06a227c
Nate Abele <nate.abele@gmail.com>
2011-02-15 12:14:40 -0500

    Fixing formatting of `ErrorController`.

--- app/controllers/ErrorController.php
+++ app/controllers/ErrorController.php
@@ -10,21 +10,17 @@ class ErrorController extends \lithium\action\Controller {
 
 	public function index() {
 		$exception = new ClassNotFoundException("Class 'Foo' of type 'bar' not found.");
+		$line = $exception->getLine();
+
 		/**
 		 * Should these be called from the view or go into a helper?
 		 * I wasn't sure, but I think it would be cleaner if the
 		 * exception was the only thing passed to the view.
 		 */
-		$sourceCode = Inspector::lines($exception->getFile(),
-			range(
-				$exception->getLine()-3,
-				$exception->getLine()+3
-			)
-		);
+		$sourceCode = Inspector::lines($exception->getFile(), range($line - 3, $line + 3));
 		$stackTrace = ErrorHandler::trace($exception->getTrace());
 		$this->set(compact('exception', 'sourceCode', 'stackTrace'));
 	}
-
 }
 
 ?>
