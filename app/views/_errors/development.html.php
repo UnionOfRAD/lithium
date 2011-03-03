6cf5cd30b09b9fce9c79b10761b773bb0702b7ed
Nate Abele <nate.abele@gmail.com>
2011-03-03 03:03:31 -0500

    Refactoring source code rendering on exception page.

--- app/views/_errors/development.html.php
+++ app/views/_errors/development.html.php
@@ -10,10 +10,11 @@ use lithium\analysis\Debugger;
 use lithium\analysis\Inspector;
 
 $exception = $info['exception'];
+$replace = array('&lt;?php', '?&gt;', '<code>', '</code>', "\n");
+$context = 5;
 
 /**
  * Set Lithium-esque colors for syntax highlighing.
- * Do these belong here? Maybe they should move to the layout or error bootstrap?
  */
 ini_set('highlight.string', '#4DDB4A');
 ini_set('highlight.comment', '#D42AAE');
@@ -21,82 +22,20 @@ ini_set('highlight.keyword', '#D42AAE');
 ini_set('highlight.default', '#3C96FF');
 ini_set('highlight.htm', '#FFFFFF');
 
-/**
- * Helper function for generating line numbered code output with syntax highlighting.
- *
- * Should this function belong in a helper or something?
- *
- * @see lithium\analysis\Inspector::lines()
- * @param string $file Normally a full path to a specific file
- * @param integer $line The central line number to retrieve
- */
-function renderCodeExcerpt($file = null, $line = null) {
+$stack = Debugger::trace(array('format' => 'array', 'trace' => $exception->getTrace()));
 
-	$sourceCode = Inspector::lines($file, range($line - 3, $line + 3));
+array_unshift($stack, array(
+	'functionRef' => '[exception]',
+	'file' => $exception->getFile(),
+	'line' => $exception->getLine()
+));
 
-	?>
-
-	<div class="lithium-exception-location">
-		<?php echo "{$file}: {$line}"; ?>
-	</div>
-
-	<div class="lithium-code-dump">
-
-		<?php
-		/**
-		 * We can get rid of the 'code' HTML elements if we prefer a lighter background
-		 * color. Also, it bothers me that the syntax highlighting isn't an exact match
-		 * with li3_docs. The highlight color can make the code a little hard to read.
-		 * If we stick with the dark background color, we might want to adjust the
-		 * highlight color.
-		 */
-		?>
-
-		<pre><code><?php
-
-			foreach ($sourceCode as $num => $content) :
-
-				// Pad the line number.
-				// We shouldn't have any files longer than 999 lines, right? ;)
-				$numPad = str_pad($num, 3, ' ');
-
-				// Nuke any existing PHP start or end tags
-				$content = str_ireplace(array('<?php', '?>'), '', $content);
-
-				// Add the line number and wrap in PHP start and end tags
-				$content = "<?php {$numPad}{$content} ?>";
-
-				// Apply syntax highlighting
-				$content = highlight_string($content, true);
-
-				// Clean out PHP start and end tags, 'code' elements, and newlines
-				$content = str_replace(array('&lt;?php', '?&gt;', '<code>', '</code>', "\n"), '', $content);
-
-				if ($line === $num):
-					?><span class="code-highlight"><?php
-				endif;?><?php echo "{$content}\n"; ?><?php
-
-				if ($line === $num):
-					?></span><?php
-				endif;
-
-			endforeach;
-
-		?></code></pre>
-
-	</div><?php
-
-}
-
-?>
-
-<?php
 /**
  * It would be cool if the name of the exception was a link to the API.
- * Also, should we show the exception code? That might be helpful for HTTP exceptions.
  */
 ?>
 <h3>Exception</h3>
+
 <div class="lithium-exception-class">
 	<?=get_class($exception);?>
 
@@ -104,33 +43,51 @@ function renderCodeExcerpt($file = null, $line = null) {
 		<span class="code">(code <?=$code; ?>)</span>
 	<?php endif ?>
 </div>
-<div class="lithium-exception-message"><?=$exception->getMessage();?></div>
+
+<div class="lithium-exception-message"><?=$exception->getMessage(); ?></div>
 
 <h3 id="source">Source</h3>
-<div id="sourceCode">
-	<?php renderCodeExcerpt($exception->getFile(), $exception->getLine()); ?>
-</div>
 
-<?php
-/**
- * In the future, maybe we'll add a 'Show All' link or button. How would that work?
- * Also we plan to improve the way that closures are displayed in the stack trace.
- */
-?>
+<div id="sourceCode"></div>
+
 <h3>Stack Trace</h3>
+
 <div class="lithium-stack-trace">
-	<?php // Should this list be zero based? ?>
 	<ol>
-		<li>
-			<?php // TODO: What should this link title be? ?>
-			<tt><a href="#source" id="0" class="display-source-excerpt">[Exception Thrown]</a></tt>
-			<div id="sourceCode0" style="display: none;"></div>
-		</li>
-		<?php foreach (Debugger::trace(array('format' => 'array', 'trace' => $exception->getTrace())) as $id => $frame) : ?>
+		<?php foreach ($stack as $id => $frame): ?>
+			<?php
+				$location = "{$frame['file']}: {$frame['line']}";
+				$lines = range($frame['line'] - $context, $frame['line'] + $context);
+				$code = Inspector::lines($frame['file'], $lines);
+			?>
 			<li>
-				<tt><a href="#source" id="<?=$id + 1;?>" class="display-source-excerpt"><?=$frame['functionRef'];?></a></tt>
-				<div id="sourceCode<?=$id + 1;?>" style="display: none;">
-					<?php renderCodeExcerpt($frame['file'], $frame['line']); ?>
+				<tt><a href="#source" id="<?=$id; ?>" class="display-source-excerpt">
+					<?=$frame['functionRef']; ?>
+				</a></tt>
+				<div id="sourceCode<?=$id; ?>" style="display: none;">
+
+					<div class="lithium-exception-location">
+						<?=$location; ?>
+					</div>
+
+					<div class="lithium-code-dump">
+						<pre><code><?php
+							foreach ($code as $num => $content):
+								$numPad = str_pad($num, 3, ' ');
+								$content = str_ireplace(array('<?php', '?>'), '', $content);
+								$content = highlight_string("<?php {$numPad}{$content} ?>", true);
+								$content = str_replace($replace, '', $content);
+
+								if ($frame['line'] === $num):
+									?><span class="code-highlight"><?php
+								endif;?><?php echo "{$content}\n"; ?><?php
+								if ($frame['line'] === $num):
+									?></span><?php
+								endif;
+
+							endforeach;
+						?></code></pre>
+					</div>
 				</div>
 			</li>
 		<?php endforeach; ?>
@@ -139,21 +96,16 @@ function renderCodeExcerpt($file = null, $line = null) {
 
 <script type="text/javascript">
 	window.onload = function() {
-		// Copy the original source excerpt so we can re-display it when neccesary
-		var content = document.getElementById('sourceCode').innerHTML;
-		document.getElementById('sourceCode0').innerHTML = content;
-
-		// Apply click event handlers
+		var $ = function() { return document.getElementById.apply(document, arguments); };
 		var links = document.getElementsByTagName('a');
+
 		for (i = 0; i < links.length; i++) {
-			// Only apply this to links with the 'display-source-excerpt' class
 			if (links[i].className.indexOf('display-source-excerpt') >= 0) {
 				links[i].onclick = function() {
-					var elementId = 'sourceCode' + this.id;
-					var content = document.getElementById(elementId).innerHTML;
-					document.getElementById('sourceCode').innerHTML = content;
+					$('sourceCode').innerHTML = $('sourceCode' + this.id).innerHTML;
 				}
 			}
 		}
+		$('sourceCode').innerHTML = $('sourceCode0').innerHTML;
 	}
 </script>
