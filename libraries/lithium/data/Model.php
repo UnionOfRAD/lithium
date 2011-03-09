58bf43907001295fa23919863e7ad36b44e6c9df
Nate Abele <nate.abele@gmail.com>
2011-03-09 00:14:30 -0500

    Adding documentation to `\data\Model::validates()`.

--- libraries/lithium/data/Model.php
+++ libraries/lithium/data/Model.php
@@ -773,12 +773,46 @@ class Model extends \lithium\core\StaticObject {
 	}
 
 	/**
-	 * Indicates whether the `Model`'s current data validates, given the
-	 * current rules setup.
+	 * An important part of describing the business logic of a model class is defining the
+	 * validation rules. In Lithium models, rules are defined through the `$validates` class
+	 * property, and are used by this method before saving to verify the correctness of the data
+	 * being sent to the backend data source.
 	 *
-	 * @param string $entity Model record to validate.
-	 * @param array $options Options.
-	 * @return boolean Success.
+	 * Note that these are application-level validation rules, and do not
+	 * interact with any rules or constraints defined in your data source. If such constraints fail,
+	 * an exception will be thrown by the database layer. The `validates()` method only checks
+	 * against the rules defined in application code.
+	 *
+	 * This method uses the `Validator` class to perform data validation. An array representation of
+	 * the entity object to be tested is passed to the `check()` method, along with the model's
+	 * validation rules. Any rules defined in the `Validator` class can be used to validate fields.
+	 * See the `Validator` class to add custom rules, or override built-in rules.
+	 *
+	 * @see lithium\data\Model::$validates
+	 * @see lithium\util\Validator::check()
+	 * @see lithium\data\Entity::errors()
+	 * @param string $entity Model entity to validate. Typically either a `Record` or `Document`
+	 *               object. In the following example:
+	 * {{{
+	 * $post = Posts::create($data);
+	 * $success = $post->validates();
+	 * }}}
+	 * The `$entity` parameter is equal to the `$post` object instance.
+	 * @param array $options Available options:
+	 *              - `'rules'` _array_: If specified, this array will _replace_ the default
+	 *                validation rules defined in `$validates`.
+	 *              - `'events'` _mixed_: A string or array defining one or more validation
+	 *                 _events_. Events are different contexts in which data events can occur, and
+	 *                correspond to the optional `'on'` key in validation rules. For example, by
+	 *                default, `'events'` is set to either `'create'` or `'update'`, depending on
+	 *                whether `$entity` already exists. Then, individual rules can specify
+	 *                `'on' => 'create'` or `'on' => 'update'` to only be applied at certain times.
+	 *                Using this parameter, you can set up custom events in your rules as well, such
+	 *                as `'on' => 'login'`. Note that when defining validation rules, the `'on'` key
+	 *                can also be an array of multiple events.
+	 * @return boolean Returns `true` if all validation rules on all fields succeed, otherwise
+	 *         `false`. After validation, the messages for any validation failures are assigned to
+	 *         the entity, and accessible through the `errors()` method of the entity object.
 	 */
 	public function validates($entity, array $options = array()) {
 		$defaults = array(
