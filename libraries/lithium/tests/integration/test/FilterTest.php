ef26466ac29ae3cf5d7b49cc33335511a34a0328
David Persson <davidpersson@gmx.de>
2011-03-23 22:21:06 +0100

    Refactoring to conform to standards.

--- libraries/lithium/tests/integration/test/FilterTest.php
+++ libraries/lithium/tests/integration/test/FilterTest.php
@@ -38,13 +38,12 @@ class FilterTest extends \lithium\test\Integration {
 
 	public function testSingleTestWithMultipleFilters() {
 		$all = array(
-			"Coverage",
-			"Complexity",
-			"Profiler",
-			"Affected"
+			'Coverage',
+			'Complexity',
+			'Profiler',
+			'Affected'
 		);
-
-		$permutations = $this->power_perms($all);
+		$permutations = $this->_powerPerms($all);
 
 		foreach ($permutations as $filters) {
 			$filters = array_flip($filters);
@@ -81,88 +80,87 @@ class FilterTest extends \lithium\test\Integration {
 		}
 	}
 
-	/**
-	 * Methods for getting all permutations of each set in the power set of an array of strings
-	 * (from the php.net manual on shuffle)
+	/*
+	 * Methods for getting all permutations of each set in the power set of an
+	 * array of strings (from the php.net manual on shuffle).
+	 *
+	 * @todo This needs further refactoring.
 	 */
 
-	private function power_perms($arr) {
-	    $power_set = $this->power_set($arr);
-	    $result = array();
-	    foreach($power_set as $set) {
-	        $perms = $this->perms($set);
-	        $result = array_merge($result,$perms);
-	    }
-	    return $result;
+	protected function _powerPerms($arr) {
+		$powerSet = $this->_powerSet($arr);
+		$result = array();
+
+		foreach ($powerSet as $set) {
+			$perms = $this->_perms($set);
+			$result = array_merge($result,$perms);
+		}
+		return $result;
 	}
 
-	private function power_set($in,$minLength = 1) {
-	   $count = count($in);
-	   $members = pow(2,$count);
-	   $return = array();
-	   for ($i = 0; $i < $members; $i++) {
-	      $b = sprintf("%0" . $count . "b", $i);
-	      $out = array();
-	      for ($j = 0; $j < $count; $j++) {
-	         if ($b{$j} == '1') $out[] = $in[$j];
-	      }
-	      if (count($out) >= $minLength) {
-	         $return[] = $out;
-	      }
-	   }
+	protected function _powerSet($in, $minLength = 1) {
+		$count = count($in);
+		$members = pow(2, $count);
+		$return = array();
 
-	   //usort($return,"cmp");  //can sort here by length
-	   return $return;
+		for ($i = 0; $i < $members; $i++) {
+			$b = sprintf("%0{$count}b", $i);
+			$out = array();
+
+			for ($j = 0; $j < $count; $j++) {
+				if ($b[$j] == '1') {
+					$out[] = $in[$j];
+				}
+			}
+			if (count($out) >= $minLength) {
+				$return[] = $out;
+			}
+		}
+		return $return;
 	}
 
-	private function factorial($int){
-	   if($int < 2) {
-	       return 1;
-	   }
-
-	   for($f = 2; $int - 1 > 1; $f *= $int--){}
-
-	   return $f;
+	protected function _factorial($int) {
+		if ($int < 2) {
+			return 1;
+		}
+		for ($f = 2; $int - 1 > 1; $f *= $int--) {}
+		return $f;
 	}
 
-	private function perm($arr, $nth = null) {
+	protected function _perm($arr, $nth = null) {
+		if ($nth === null) {
+			return $this->_perms($arr);
+		}
+		$result = array();
+		$length = count($arr);
 
-	    if ($nth === null) {
-	        return $this->perms($arr);
-	    }
-
-	    $result = array();
-	    $length = count($arr);
-
-	    while ($length--) {
-	        $f = $this->factorial($length);
-	        $p = floor($nth / $f);
-	        $result[] = $arr[$p];
-	        $this->array_delete_by_key($arr, $p);
-	        $nth -= $p * $f;
-	    }
-
-	    $result = array_merge($result,$arr);
-	    return $result;
+		while ($length--) {
+			$f = $this->_factorial($length);
+			$p = floor($nth / $f);
+			$result[] = $arr[$p];
+			$this->_arrayDeleteByKey($arr, $p);
+			$nth -= $p * $f;
+		}
+		$result = array_merge($result,$arr);
+		return $result;
 	}
 
-	private function perms($arr) {
-	    $p = array();
-	    for ($i = 0; $i < $this->factorial(count($arr)); $i++) {
-	        $p[] = $this->perm($arr, $i);
-	    }
-	    return $p;
+	protected function _perms($arr) {
+		$p = array();
+
+		for ($i = 0; $i < $this->_factorial(count($arr)); $i++) {
+			$p[] = $this->_perm($arr, $i);
+		}
+		return $p;
 	}
 
-	private function array_delete_by_key(&$array, $delete_key, $use_old_keys = FALSE) {
+	protected function _arrayDeleteByKey(&$array, $deleteKey, $useOldKeys = false) {
+		unset($array[$deleteKey]);
 
-	    unset($array[$delete_key]);
-
-	    if(!$use_old_keys) {
-	        $array = array_values($array);
-	    }
-
-	    return TRUE;
+		if (!$useOldKeys) {
+			$array = array_values($array);
+		}
+		return true;
 	}
 }
 
