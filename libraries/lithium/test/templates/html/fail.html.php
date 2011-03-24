93ed3841f3c28346c2fb99b59b21c42056aec3f8
David Persson <davidpersson@gmx.de>
2011-03-24 13:26:20 +0100

    Escape entities in assert fail message.

--- libraries/lithium/test/templates/html/fail.html.php
+++ libraries/lithium/test/templates/html/fail.html.php
@@ -2,5 +2,7 @@
 	Assertion '<?php echo $error['assertion'] ?>' failed in
 	<?php echo $error['class'] ?>::<?php echo $error['method']?>() on line
 	<?php echo $error['line'] ?>:
-	<span class="content"><?php echo $error['message'] ?></span>
+	<span class="content"><?php echo htmlspecialchars(
+		$error['message'], ENT_QUOTES, 'UTF-8'
+	) ?></span>
 </div>
