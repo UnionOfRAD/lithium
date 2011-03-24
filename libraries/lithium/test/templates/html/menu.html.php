6c4ede74bd2939a2d2bef9d224abfe42bceb80f0
David Persson <davidpersson@gmx.de>
2011-03-24 19:29:54 +0100

    Strip Test suffix from menu items.

--- libraries/lithium/test/templates/html/menu.html.php
+++ libraries/lithium/test/templates/html/menu.html.php
@@ -48,7 +48,11 @@
 		?>
 	<?php endwhile ?>
 
-	<li><a href="<?php echo $base ?>/test/<?php echo join($path, "/") ?>/<?php echo $case ?>" title="run <?php echo $case; ?>"><?php echo $case ?></a></li>
+	<li>
+		<a href="<?php echo $base ?>/test/<?php echo join($path, "/") ?>/<?php echo $case ?>" title="run <?php echo $case; ?>">
+			<?php echo preg_replace('/Test$/', null, $case); ?>
+		</a>
+	</li>
 
 <?php endforeach ?>
 
