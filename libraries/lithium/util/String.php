4081de033d40c02b92ed499f2ed790f33e9632b7
Nate Abele <nate.abele@gmail.com>
2011-06-04 10:11:55 -0400

    Reformatting `\util\String::uuid()` and associated constants.

--- libraries/lithium/util/String.php
+++ libraries/lithium/util/String.php
@@ -17,35 +17,45 @@ use lithium\security\Crypto;
  * {:tag} and regex replacement, and tokenization.
  */
 class String {
+
 	/**
-	 * UUID related constants
+	 * UUID-related constant. Clears all bits of version byte (`00001111`).
 	 */
-	const clearVer = 15;  // 00001111  Clears all bits of version byte
-	const version4 = 64;  // 01000000  Sets the version bit
-	const clearVar = 63;  // 00111111  Clears relevant bits of variant byte
-	const varRFC   = 128; // 10000000  The RFC 4122 variant
+	const UUID_CLEAR_VER = 15;
+
+	/**
+	 * UUID constant that sets the version bit for generated UUIDs (`01000000`).
+	 */
+	const UUID_VERSION_4 = 64;
+
+	/**
+	 * Clears relevant bits of variant byte (`00111111`).
+	 */
+	const UUID_CLEAR_VAR = 63;
+
+	/**
+	 * The RFC 4122 variant (`10000000`).
+	 */
+	const UUID_VAR_RFC = 128;
 
 	/**
 	 * Generates an RFC 4122-compliant version 4 UUID.
 	 *
 	 * @return string The string representation of an RFC 4122-compliant, version 4 UUID.
-	 * @link http://www.ietf.org/rfc/rfc4122.txt
+	 * @link http://www.ietf.org/rfc/rfc4122.txt RFC 4122: UUID URN Namespace
 	 */
 	public static function uuid() {
 		$uuid = Crypto::random(16);
+		$uuid[6] = chr(ord($uuid[6]) & static::UUID_CLEAR_VER | static::UUID_VERSION_4);
+		$uuid[8] = chr(ord($uuid[8]) & static::UUID_CLEAR_VAR | static::UUID_VAR_RFC);
 
-		// Set version
-		$uuid[6] = chr(ord($uuid[6]) & static::clearVer | static::version4);
-
-		// Set variant
-		$uuid[8] = chr(ord($uuid[8]) & static::clearVar | static::varRFC);
-
-		// Return the uuid's string representation
-		return bin2hex(substr($uuid, 0, 4)) . '-'
-			. bin2hex(substr($uuid, 4, 2)) . '-'
-			. bin2hex(substr($uuid, 6, 2)) . '-'
-			. bin2hex(substr($uuid, 8, 2)) . '-'
-			. bin2hex(substr($uuid, 10, 6));
+		return join('-', array(
+			bin2hex(substr($uuid, 0, 4)),
+			bin2hex(substr($uuid, 4, 2)),
+			bin2hex(substr($uuid, 6, 2)),
+			bin2hex(substr($uuid, 8, 2)),
+			bin2hex(substr($uuid, 10, 6))
+		));
 	}
 
 	/**
@@ -55,7 +65,6 @@ class String {
 	public static function hash($string, array $options = array()) {
 		return Crypto::hash($string, $options);
 	}
-
 
 	/**
 	 * Replaces variable placeholders inside a string with any given data. Each key
