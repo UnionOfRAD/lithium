a1b8d8dc9cd3d7ce8f76e137aa7f4cd74c67029f
Paul Webster <paul@webprogression.co.nz>
2011-06-11 22:57:50 +1200

    Correctly apply Mysql result iteration, and implment required `prev()` method

--- data/source/database/adapter/my_sql/Result.php
+++ data/source/database/adapter/my_sql/Result.php
@@ -8,10 +8,32 @@
 
 namespace lithium\data\source\database\adapter\my_sql;
 
+use lithium\data\source\database;
+
 class Result extends \lithium\data\source\database\Result {
 
+	public function prev() {
+		if ($this->_current = $this->_prev()) {
+			$this->_iterator--;
+			return $this->_current;
+		}
+	}
+
+	protected function _prev() {
+		if ($this->_resource && $this->_iterator) {
+			if(mysql_data_seek($this->_resource, $this->_iterator -1)) {
+				return mysql_fetch_row($this->_resource);
+			}
+		}
+	}
+
 	protected function _next() {
-		return mysql_fetch_row($this->_resource);
+		if ($this->_resource) {
+			$inRange = $this->_iterator < mysql_num_rows($this->_resource);
+			if($inRange && mysql_data_seek($this->_resource, $this->_iterator)) {
+				return mysql_fetch_row($this->_resource);
+			}
+		}
 	}
 
 	protected function _close() {
