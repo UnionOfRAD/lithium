b48a31323c033e7e4a79b6486fcbc2603a8e4458
Michael Nitschinger <michael@nitschinger.at>
2011-07-08 20:55:09 +0200

    adding ANY to dns record check which prevents false-negative cases.

--- tests/cases/console/command/LibraryTest.php
+++ tests/cases/console/command/LibraryTest.php
@@ -409,7 +409,7 @@ class LibraryTest extends \lithium\test\Unit {
 		$this->skipIf(strpos(shell_exec('git --version'), 'git version') === false,
 			'Git is not installed.'
 		);
-		$this->skipIf(dns_check_record("lithify.me") === false, "No internet connection.");
+		$this->skipIf(dns_check_record("lithify.me", "ANY") === false, "No internet connection.");
 
 		$this->library->path = $this->_testPath;
 		$result = $this->library->install('li3_docs');
