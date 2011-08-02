UnionOfRAD/lithium

commit f2a44de15a3fe8a5bf8a3c107b8a0cf419d03f4a

Fix setting Request CONTENT_TYPE from the environment settings

net/http/Message.php
@@ -133,7 +133,7 @@ class Message extends \lithium\net\Message {
 	 *         on the content type of the request.
 	 */
 	public function type($type = null) {
-		if ($type === null) {
+		if ($type == null) {
 			return $this->_type;
 		}
 		if (strpos($type, '/')) {

commit 97e023b4220d7cd767384b0f1aed09909c53781c

Fix regression in Response::type() introduced in f2a44de15a3fe8a5bf8a

net/http/Message.php
@@ -133,7 +133,7 @@ class Message extends \lithium\net\Message {
 	 *         on the content type of the request.
 	 */
 	public function type($type = null) {
-		if ($type == null) {
+		if ($type == null && $type !== false) {
 			return $this->_type;
 		}
 		if (strpos($type, '/')) {