066d41b92b6a3b478d409cc64063e57a7a65ab56
Nate Abele <nate.abele@gmail.com>
2011-08-14 22:59:22 -0400

    Rewriting console bootstrap file to do a slightly better job detecting when a library can self-bootstrap.

--- console/lithium.php
+++ console/lithium.php
@@ -16,21 +16,14 @@
  * application context is found, just boot up the core framework.
  */
 $params = getopt("", array("app::"));
-$working = !empty($params) ? array_pop($params) : getcwd();
+$working = $params ? array_pop($params) : getcwd();
 $app = null;
 
-if (file_exists($working . '/config/bootstrap.php')) {
-	$app = $working;
-} elseif (file_exists($working . '/app/config/bootstrap.php')) {
-	$app = $working . '/app';
-}
-
 /**
- * Bootstrap the console.
+ * If we're not running inside an application (i.e. a self-bootstrapping library), bootstrap the
+ * core automatically with the default settings.
  */
-if ($app && file_exists("{$app}/config/bootstrap/libraries.php")) {
-	include "{$app}/config/bootstrap.php";
-} else {
+$bootstrap = function() use ($working) {
 	define('LITHIUM_LIBRARY_PATH', dirname(dirname(__DIR__)));
 	define('LITHIUM_APP_PATH', $working);
 
@@ -40,12 +33,13 @@ if ($app && file_exists("{$app}/config/bootstrap/libraries.php")) {
 		$message .= "/libraries directory.";
 		throw new ErrorException($message);
 	}
+
 	lithium\core\Libraries::add('lithium');
 	lithium\core\Libraries::add(basename($working), array(
 		'default' => true,
 		'path' => $working
 	));
-}
+};
 
 /**
  * The following will dispatch the request and exit with the status code as
@@ -70,6 +64,42 @@ if ($app && file_exists("{$app}/config/bootstrap/libraries.php")) {
  * @see lithium\console\Dispatcher
  * @see lithium\console\Router
  */
-exit(lithium\console\Dispatcher::run(new lithium\console\Request())->status);
+$run = function() {
+	return lithium\console\Dispatcher::run(new lithium\console\Request())->status;
+};
+
+/**
+ * Look to see if there's a bootstrap file. If there is, this is either a Lithium application or
+ * plugin.
+ */
+if (file_exists("{$working}/config/bootstrap.php")) {
+	$app = $working;
+} elseif (file_exists("{$working}/app/config/bootstrap.php")) {
+	$app = "{$working}/app";
+}
+
+/**
+ * Attempt to bootstrap the application and execute the request. On failure, use the default
+ * bootstrap.
+ */
+if ($app) {
+	foreach (array("bootstrap.php", "bootstrap/libraries.php") as $file) {
+		if (!file_exists($path = "{$app}/config/{$file}")) {
+			continue;
+		}
+		if (preg_match("/^define\([\"']LITHIUM_LIBRARY_PATH[\"']/m", file_get_contents($path))) {
+			include "{$app}/config/bootstrap.php";
+			exit($run());
+		}
+	}
+}
+
+/**
+ * We're not running inside a Lithium application. Use the default bootstrap and execute the
+ * request.
+ */
+$bootstrap();
+$app ? include "{$app}/config/bootstrap.php" : null;
+exit($run());
 
 ?>
