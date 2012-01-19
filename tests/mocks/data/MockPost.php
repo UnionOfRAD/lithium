ca3663738dff3b2578843a633a1cf5535795e71e
Nate Abele <nate.abele@gmail.com>
2012-01-19 02:25:20 -0500

    Finishing integration of new `Schema` object into core. Adding accessor methods for convenience. Misc. refactoring. Updating tests and mocks.

--- tests/mocks/data/MockPost.php
+++ tests/mocks/data/MockPost.php
@@ -8,18 +8,23 @@
 
 namespace lithium\tests\mocks\data;
 
+use lithium\data\Schema;
+
 class MockPost extends \lithium\tests\mocks\data\MockBase {
 
 	public $hasMany = array('MockComment');
 
 	public static $connection = null;
 
-	public static function resetSchema() {
-		static::_object()->_schema = array();
+	public static function resetSchema($array = false) {
+		if ($array) {
+			static::_object()->_schema = array();
+		}
+		static::_object()->_schema = new Schema();
 	}
 
-	public static function overrideSchema(array $schema = array()) {
-		static::_object()->_schema = $schema;
+	public static function overrideSchema(array $fields = array()) {
+		static::_object()->_schema = new Schema(compact('fields'));
 	}
 
 	public static function instances() {
