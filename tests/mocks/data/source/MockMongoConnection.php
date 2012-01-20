417af95d4545905269d072405ebe850918b33d18
Nate Abele <nate.abele@gmail.com>
2012-01-20 04:16:16 -0500

    Refactoring data namespace. Cutting out the middle man in mock classes to simplify testing.

--- tests/mocks/data/source/MockMongoConnection.php
+++ tests/mocks/data/source/MockMongoConnection.php
@@ -8,10 +8,50 @@
 
 namespace lithium\tests\mocks\data\source;
 
-class MockMongoConnection extends \lithium\data\source\MongoDb {
+use MongoId;
+
+class MockMongoConnection {
+
+	public $queries = array();
+
+	public $results = array();
+
+	protected $_collection = null;
 
 	public function connect() {
 		return false;
+	}
+
+	public function &__get($property) {
+		$this->_collection = $property;
+		return $this;
+	}
+
+	public function insert(array &$data, array $options = array()) {
+		$data['_id'] = new MongoId();
+		return $this->_record(__FUNCTION__, compact('data', 'options'));
+	}
+
+	protected function _record($type, array $data = array()) {
+		$collection = $this->_collection;
+		$this->queries[] = compact('type', 'collection') + $data;
+		return array_pop($this->results);
+	}
+
+	public function update($conditions, $update, $options) {
+		return $this->_record(__FUNCTION__, compact('conditions', 'update', 'options'));
+	}
+
+	public function remove($conditions, $options) {
+		return $this->_record(__FUNCTION__, compact('conditions', 'options'));
+	}
+
+	public function find($conditions, $fields) {
+		return $this->_record(__FUNCTION__, compact('conditions', 'fields'));
+	}
+
+	public function listCollections() {
+		return $this->_record(__FUNCTION__);
 	}
 }
 
