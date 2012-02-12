85b4f5ad2cdeafce7de8cdc7548f2a10895b0970
Nate Abele <nate.abele@gmail.com>
2012-02-12 17:19:17 -0500

    Build script, round 6.

--- tests/ci_depends.sh
+++ tests/ci_depends.sh
@@ -2,10 +2,10 @@
 
 wget -q http://pecl.php.net/get/APC-3.1.9.tgz
 tar -xzf APC-3.1.9.tgz
-sh -c -q "cd APC-3.1.9 && phpize && ./configure && make && sudo make install"
+sh -c "cd APC-3.1.9 && phpize && ./configure && make -s && sudo make -s install"
 echo "extension=apc.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
 
 wget -q http://pecl.php.net/get/mongo-1.2.7.tgz
 tar -xzf mongo-1.2.7.tgz
-sh -c -q "cd mongo-1.2.7 && phpize && ./configure && make && sudo make install"
+sh -c "cd mongo-1.2.7 && phpize && ./configure && make -s && sudo make -s install"
 echo "extension=mongo.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
