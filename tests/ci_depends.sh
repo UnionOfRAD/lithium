5feb569354f54dabda72b0f6a170be7361a2f571
Nate Abele <nate.abele@gmail.com>
2012-02-12 17:09:05 -0500

    Build script, round 4.

--- tests/ci_depends.sh
+++ tests/ci_depends.sh
@@ -1,17 +1,18 @@
 # Install APC and Mongo extensions
 
-echo "Installing APC...";
+echo "Installing APC..."
 wget http://pecl.php.net/get/APC-3.1.9.tgz
 tar -xzf APC-3.1.9.tgz
 sh -c "cd APC-3.1.9 && phpize && ./configure && make && sudo make install"
 echo "extension=apc.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
 
-echo "Installing Mongo...";
+echo "Installing Mongo..."
 wget http://pecl.php.net/get/mongo-1.2.7.tgz
 tar -xzf mongo-1.2.7.tgz
 sh -c "cd mongo-1.2.7 && phpize && ./configure && make && sudo make install"
 echo "extension=mongo.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
 
 # Ensure read-only mode for Phar files is disabled
-echo "Disabling Phar read-only mode...";
+echo "Disabling Phar read-only mode..."
+echo `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
 sed -E -i '' -e's/phar\.readonly = On/phar.readonly = Off/g' `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
