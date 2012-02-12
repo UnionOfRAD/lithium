496c7151777d4a816034491d2fa958c3d7f8a59a
Nate Abele <nate.abele@gmail.com>
2012-02-12 16:56:42 -0500

    Build script, round 2.

--- tests/ci_depends.sh
+++ tests/ci_depends.sh
@@ -1,7 +1,12 @@
 # Install APC and Mongo extensions
 
-sudo pecl install apc
-sudo pecl install mongo
+wget -q http://pecl.php.net/get/APC-3.1.9.tgz
+tar -xzf APC-3.1.9.tgz
+sh -c "cd APC-3.1.9 && phpize && ./configure && make && sudo make install"
+
+wget -q http://pecl.php.net/get/mongo-1.2.7.tgz
+tar -xzf mongo-1.2.7.tgz
+sh -c "cd mongo-1.2.7 && phpize && ./configure && make && sudo make install"
 
 echo "extension=apc.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
 echo "extension=mongo.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
