a5319d78daa6ad97bb0832a040c8c02bed7c9373
Nate Abele <nate.abele@gmail.com>
2012-02-13 12:11:39 -0500

    Retrying the build script thang with some Perl.

--- tests/ci_depends.sh
+++ tests/ci_depends.sh
@@ -9,3 +9,6 @@ wget -q http://pecl.php.net/get/mongo-1.2.7.tgz
 tar -xzf mongo-1.2.7.tgz
 sh -c "cd mongo-1.2.7 && phpize && ./configure && make -s && sudo make -s install"
 echo "extension=mongo.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
+
+INIFILE=`php --ini | grep "Loaded Configuration" | awk {'print $4'}`
+perl -p -i -e 's/phar\.readonly = On/phar.readonly = Off/' $INIFILE
