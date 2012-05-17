49b2ef13f350c3ff4e37e9ec5b7cf2b02dc77cdf
David Persson <davidpersson@gmx.de>
2012-05-17 22:22:12 +0200

    Formatting test profiler console text output.

--- test/templates/console/profiler.txt.php
+++ test/templates/console/profiler.txt.php
@@ -1,6 +1,9 @@
 {:heading}Benchmarks{:end}
 <?php
+	$width = array_reduce(array_keys($data['totals']), function($v, $w) {
+		return $v = (($l = strlen($w)) > $v) ? $l : $v;
+	});
 	foreach ($data['totals'] as $title => $result) {
-		echo "{$title}: {$result['formatter']($result['value'])}\n";
+		printf("%-{$width}s   %s\n", $title, $result['formatter']($result['value']));
 	}
 ?>
