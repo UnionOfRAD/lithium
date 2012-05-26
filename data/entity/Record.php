544a77c5a52b5e0c25d22325975fdf5d80d6b795
Simon JAILLET <sjaillet@gmail.com>
2012-05-26 21:18:21 +0200

    Fix #206

--- data/entity/Record.php
+++ data/entity/Record.php
@@ -13,6 +13,21 @@ namespace lithium\data\entity;
  * as columns in databases).
  */
 class Record extends \lithium\data\Entity {
+
+	/**
+	 * Converts a `Record` object to another specified format.
+	 *
+	 * @param string $format The format used by default is `array`
+	 * @param array $options
+	 * @return mixed
+	 */
+	public function to($format, array $options = array()) {
+		$defaults = array('handlers' => array(
+			'stdClass' => function($item) { return $item;}
+		));
+		$options += $defaults;
+		return parent::to($format, $options);
+	}
 }
 
 ?>
