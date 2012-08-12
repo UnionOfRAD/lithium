9a156500f422847d2adb9a0e0237ec92211ab9ab
Chris T <cortone@gmail.com>
2012-08-13 00:01:03 +0300

    Fix the fix for the editable id column
    With the previous fix i managed to break the create, it should be fine now

--- data/Entity.php
+++ data/Entity.php
@@ -406,17 +406,14 @@ class Entity extends \lithium\core\Object {
 	 * @param string $name Optionally included field name.
 	 * @return mixed Entire stored data array if $name is empty, otherwise the value from the named field.
          */
-        public function stored($name = null)
-        {
-            if($this->exists())
-            {
-                if($name)
-                {
+        public function stored($name = null){
+            if($this->exists()) {
+                if($name) {
                     return isset($this->_data[$name]) ? $this->_data[$name] : null;
                 }
                 return $this->_data;
             }
-            return false;
+            return $this->data($name);
         }
 
 	/**
