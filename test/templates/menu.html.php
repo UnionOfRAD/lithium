ae7df4b3d6806c3c576dcc2ffdcce2183602d2a0
Simon JAILLET <sjaillet@gmail.com>
2012-12-04 01:26:48 +0100

    Simiplfying test menu generation Fix.

--- test/templates/menu.html.php
+++ test/templates/menu.html.php
@@ -7,13 +7,12 @@ $render = function($self, $path, $parent = null) use ($base) {
 			continue;
 		}
 		echo "<li><a class='menu-folder' title='run {$parent}\{$current}'
-			href='{$base}/{$parent}/{$current}'>{$current}</a></li>";
-		echo "<ul>";
-		$self($value, $current, $render);
-		echo "<ul>";
+			href='{$base}/{$parent}/{$current}'>{$current}</a><ul>";
+		$self($self, $value, $parent . '/' . $current);
+		echo "</ul></li>";
 	}
 };
 echo "<ul>";
-$render($render, $menu);
-echo "<ul>";
+$render($render, $menu, 'test');
+echo "</ul>";
 ?>
