11ccfc1836b42d0cdaab43531305157b6518cba6
gwoo <gwoohoo@gmail.com>
2012-12-03 17:07:19 -0800

    Fixing formatting of test menu.

--- test/templates/menu.html.php
+++ test/templates/menu.html.php
@@ -1,22 +1,24 @@
 <?php
 
 $render = function($self, $path, $parent = null) use ($base) {
+	$result = array("<ul class='menu'>");
+
 	foreach ($path as $current => $value) {
 		$path = trim(str_replace("//", "/", "{$parent}/{$current}"), "/");
+		$result[] = "<li>";
 
 		if (is_string($value)) {
-			echo  "<li><a title='run {$path}'
-			href='{$base}/test/{$path}'>{$current}</a></li>";
+			$result[] =  "<a title='run {$path}' "
+				. "href='{$base}/test/{$path}'>{$current}</a>";
 			continue;
 		}
-		echo "<li><a class='menu-folder' title='run {$path}'
-			href='{$base}/test/{$path}'>{$current}</a></li>";
-		echo "<ul>";
-		$self($self, $value, $path);
-		echo "<ul>";
+		$result[] =  "<a class='menu-folder' title='run {$path}' "
+			. "href='{$base}/test/{$path}'>{$current}</a>";
+		$result[] = $self($self, $value, $path);
+		$result[] = "</li>";
 	}
+	$result[] = "</ul>";
+	return join("\n", $result);
 };
-echo "<ul>";
-$render($render, $menu);
-echo "<ul>";
+echo $render($render, $menu);
 ?>
