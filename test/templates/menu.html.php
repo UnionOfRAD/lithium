e9091aaf7b785e9ebe3eebe0e0b397311a83a841
gwoo <gwoohoo@gmail.com>
2012-12-03 16:44:17 -0800

    Fixing errors in test menu.

--- test/templates/menu.html.php
+++ test/templates/menu.html.php
@@ -1,15 +1,18 @@
 <?php
+
 $render = function($self, $path, $parent = null) use ($base) {
 	foreach ($path as $current => $value) {
+		$path = trim(str_replace("//", "/", "{$parent}/{$current}"), "/");
+
 		if (is_string($value)) {
-			echo  "<li><a title='run {$parent}\{$current}'
-			href='{$base}/{$parent}/{$current}'>{$current}</a></li>";
+			echo  "<li><a title='run {$path}'
+			href='{$base}/test/{$path}'>{$current}</a></li>";
 			continue;
 		}
-		echo "<li><a class='menu-folder' title='run {$parent}\{$current}'
-			href='{$base}/{$parent}/{$current}'>{$current}</a></li>";
+		echo "<li><a class='menu-folder' title='run {$path}'
+			href='{$base}/test/{$path}'>{$current}</a></li>";
 		echo "<ul>";
-		$self($value, $current, $render);
+		$self($self, $value, $path);
 		echo "<ul>";
 	}
 };
