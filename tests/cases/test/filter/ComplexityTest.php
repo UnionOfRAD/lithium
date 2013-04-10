2915c338be7a084a9d8e1c0a81337f47acc0851e
Blaine Schmeisser <blaine.schmeisser@vitals.com>
2013-04-09 20:24:10 -0500

    Update `Complexity` tests to stub dependencies.
    Conflicts:
    	tests/cases/test/filter/ComplexityTest.php

--- tests/cases/test/filter/ComplexityTest.php
+++ tests/cases/test/filter/ComplexityTest.php
@@ -9,8 +9,12 @@
 namespace lithium\tests\cases\test\filter;
 
 use lithium\test\filter\Complexity;
-use lithium\test\Group;
 use lithium\test\Report;
+use lithium\test\Mocker;
+use lithium\util\collection\Mock as CollectionMock;
+use lithium\analysis\parser\Mock as ParserMock;
+use lithium\analysis\inspector\Mock as InspectorMock;
+use lithium\test\group\Mock as GroupMock;
 
 /**
  * The `ComplexityTest` class tests the `Complexity` filter which calculates the cyclomatic
@@ -26,21 +30,59 @@ class ComplexityTest extends \lithium\test\Unit {
 	 */
 	protected $_paths = array(
 		'complexity' => 'lithium\test\filter\Complexity',
-		'testClass' => 'lithium\core\StaticObject',
-		'testClassTest' => 'lithium\tests\cases\core\StaticObjectTest'
+		'testClass' => 'FooObject',
+		'testClassTest' => 'FooObjectTest'
 	);
 
 	/**
 	 * Helper array which stores the expected results to clean up the tests.
 	 */
 	protected $_metrics = array(
-		'invokeMethod' => 8,
-		'_filter' => 3,
-		'applyFilter' => 5,
-		'_parents' => 2,
-		'_instance' => 2,
-		'_stop' => 1,
-		'respondsTo' => 1,
+		array(
+			'FooObject' => array(
+				'applyFilter' => 5,
+			)
+		),
+		array(
+			'FooObject' => array(
+				'invokeMethod' => 8,
+				'applyFilter' => 5,
+			)
+		),
+		array(
+			'FooObject' => array(
+				'invokeMethod' => 8,
+				'applyFilter' => 5,
+				'_instance' => 2,
+			)
+		),
+		array(
+			'FooObject' => array(
+				'invokeMethod' => 8,
+				'applyFilter' => 5,
+				'_instance' => 2,
+				'_filter' => 3,
+			)
+		),
+		array(
+			'FooObject' => array(
+				'invokeMethod' => 8,
+				'applyFilter' => 5,
+				'_instance' => 2,
+				'_filter' => 3,
+				'_parents' => 2,
+			)
+		),
+		array(
+			'FooObject' => array(
+				'invokeMethod' => 8,
+				'applyFilter' => 5,
+				'_instance' => 2,
+				'_filter' => 3,
+				'_parents' => 2,
+				'_stop' => 1,
+			)
+		),
 	);
 
 	/**
@@ -50,24 +92,55 @@ class ComplexityTest extends \lithium\test\Unit {
 	 */
 	public function setUp() {
 		$this->report = new Report();
+		Mocker::register();
+		Mocker::overwriteFunction(false);
+	}
+
+	public function tearDown() {
+		Mocker::overwriteFunction(false);
 	}
 
 	/**
 	 * Tests the `apply` method which provides a high-level interface to the complexity generation.
-	 * It tests the cyclomatic complexity of the lithium\core\StaticObject class and its methods.
+	 * It tests the cyclomatic complexity of the FooObject class and its methods.
 	 *
 	 * @see lithium\test\filter\Complexity::apply()
 	 */
 	public function testApply() {
 		extract($this->_paths);
 
-		$group = new Group();
+		$collection = new CollectionMock();
+		$group = new GroupMock();
 		$group->add($testClassTest);
 		$this->report->group = $group;
 
-		Complexity::apply($this->report, $group->tests());
+		InspectorMock::applyFilter('methods', function($self, $params, $chain) {
+			return array('foo' => array(1), 'bar' => array(2)); // return 2 methods
+		});
+		InspectorMock::applyFilter('lines', function($self, $params, $chain) {
+			return 'return;'; // return a single return
+		});
+		ParserMock::applyFilter('tokenize', function($self, $params, $chain) {
+			return array(1,2,3); // always return 3 methods
+		});
+		$group->applyFilter('tests', function($self, $params, $chain) use($collection) {
+			return $collection;
+		});
+		$collection->applyFilter('invoke', function($self, $params, $chain) {
+			return array('FooObject');
+		});
+
+		Complexity::apply($this->report, $group->tests(), array(
+			'classes' => array(
+				'parser' => 'lithium\analysis\parser\Mock',
+				'inspector' => 'lithium\analysis\inspector\Mock',
+			),
+		));
 		$results = array_pop($this->report->results['filters'][$complexity]);
-		$expected = array($testClass => $this->_metrics);
+		$expected = array($testClass => array(
+			'foo' => 4,
+			'bar' => 4,
+		));
 		$this->assertEqual($expected, $results);
 	}
 
@@ -80,17 +153,26 @@ class ComplexityTest extends \lithium\test\Unit {
 	public function testAnalyze() {
 		extract($this->_paths);
 
-		$group = new Group();
+		$group = new GroupMock();
 		$group->add($testClassTest);
 		$this->report->group = $group;
 
-		Complexity::apply($this->report, $group->tests());
+		$this->report->results['filters'] = array(
+			'lithium\test\filter\Complexity' => $this->_metrics,
+		);
 
 		$results = Complexity::analyze($this->report);
-		$expected = array('class' => array($testClass => 3.1));
-		foreach ($this->_metrics as $method => $metric) {
-			$expected['max'][$testClass . '::' . $method . '()'] = $metric;
-		}
+		$expected = array(
+			'class' => array($testClass => 3.5),
+			'max' => array(
+				'FooObject::invokeMethod()' => 8,
+				'FooObject::applyFilter()' => 5,
+				'FooObject::_filter()' => 3,
+				'FooObject::_parents()' => 2,
+				'FooObject::_instance()' => 2,
+				'FooObject::_stop()' => 1,
+			),
+		);
 		$this->assertEqual($expected['max'], $results['max']);
 		$result = round($results['class'][$testClass], 1);
 		$this->assertIdentical($expected['class'][$testClass], $result);
@@ -104,11 +186,15 @@ class ComplexityTest extends \lithium\test\Unit {
 	public function testCollect() {
 		extract($this->_paths);
 
-		$group = new Group();
+		$group = new GroupMock();
 		$group->add($testClassTest);
 		$this->report->group = $group;
 
-		Complexity::apply($this->report, $group->tests());
+		$this->report->results['filters'] = array(
+			'lithium\test\filter\Complexity' => array(
+				array('FooObject' => $this->_metrics),
+			),
+		);
 
 		$results = Complexity::collect($this->report->results['filters'][$complexity]);
 		$expected = array($testClass => $this->_metrics);
