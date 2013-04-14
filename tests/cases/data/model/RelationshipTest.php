73a3499652eec05094941f807a81ff7d587bbf7d
Simon JAILLET <sjaillet@gmail.com>
2013-04-14 22:43:17 +0200

    Fix association keys generation for `Relationship` and introduce `Relationship::foreignKeys()`.

--- tests/cases/data/model/RelationshipTest.php
+++ tests/cases/data/model/RelationshipTest.php
@@ -9,6 +9,7 @@
 namespace lithium\tests\cases\data\model;
 
 use lithium\data\model\Relationship;
+use lithium\tests\mocks\data\model\MockGallery;
 
 class RelationshipTest extends \lithium\test\Unit {
 
@@ -23,6 +24,93 @@ class RelationshipTest extends \lithium\test\Unit {
 		));
 		$this->assertTrue($query->respondsTo('foobarbaz'));
 		$this->assertFalse($query->respondsTo(0));
+	}
+
+	public function testHasManyKey() {
+		$config = array(
+			'from' => $this->_gallery,
+			'to' => $this->_image,
+			'type' => 'hasMany',
+			'fieldName' => 'images',
+		);
+		$relation = new Relationship($config + array(
+			'key' => 'gallery_id'
+		));
+
+		$expected = array('id' => 'gallery_id');
+		$this->assertEqual($expected, $relation->key());
+
+		$relation = new Relationship($config + array(
+			'key' => array('id' => 'gallery_id')
+		));
+		$this->assertEqual($expected, $relation->key());
+	}
+
+	public function testBelongsToKey() {
+		$config = array(
+			'from' => $this->_gallery,
+			'to' => $this->_image,
+			'type' => 'belongsTo',
+			'fieldName' => 'images',
+		);
+		$relation = new Relationship($config + array(
+			'key' => 'gallery_id'
+		));
+
+		$expected = array('gallery_id' => 'id');
+		$this->assertEqual($expected, $relation->key());
+
+		$relation = new Relationship($config + array(
+			'key' => array('gallery_id' => 'id')
+		));
+		$this->assertEqual($expected, $relation->key());
+	}
+
+	public function testForeignKeysFromEntity() {
+		$entity = MockGallery::create(array('id' => 5));
+		$relation = MockGallery::relations('Image');
+		$this->assertEqual(array('gallery_id' => 5), $relation->foreignKey($entity));
+	}
+
+	public function testHasManyForeignKey() {
+		$config = array(
+			'from' => $this->_gallery,
+			'to' => $this->_image,
+			'type' => 'hasMany',
+			'fieldName' => 'images'
+		);
+		$relation = new Relationship($config + array(
+			'key' => 'gallery_id'
+		));
+
+		$expected = array('gallery_id' => 5);
+		$this->assertEqual($expected, $relation->foreignKey(array('id' => 5)));
+
+		$relation = new Relationship($config + array(
+			'key' => array('id' => 'gallery_id')
+		));
+		$this->assertEqual($expected, $relation->foreignKey(array('id' => 5)));
+	}
+
+	public function testBelongsToForeignKey() {
+		$config = array(
+			'from' => $this->_image,
+			'to' => $this->_gallery,
+			'type' => 'belongsTo',
+			'fieldName' => 'gallery'
+		);
+		$relation = new Relationship($config + array(
+			'key' => 'gallery_id'
+		));
+
+		$expected = array('gallery_id' => 5);
+		$this->assertEqual($expected, $relation->foreignKey(array('id' => 5)));
+
+		$relation = new Relationship($config + array(
+			'key' => array('gallery_id' => 'id')
+		));
+
+		$this->assertEqual($expected, $relation->foreignKey(array('id' => 5)));
 	}
 
 	public function testEmptyRequiredOptions() {
