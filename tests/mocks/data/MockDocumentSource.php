9411d4af91c26c4e50526889d625109ca08dc579
Simon JAILLET <sjaillet@gmail.com>
2013-07-22 15:50:45 +0200

    Add missing mock file.

---
+++ tests/mocks/data/MockDocumentSource.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Lithium: the most rad php framework
+ *
+ * @copyright     Copyright 2013, Union of RAD (http://union-of-rad.org)
+ * @license       http://opensource.org/licenses/bsd-license.php The BSD License
+ */
+
+namespace lithium\tests\mocks\data;
+
+class MockDocumentSource extends \lithium\tests\mocks\data\MockSource {
+
+	protected $_classes = array(
+		'entity' => 'lithium\data\entity\Document',
+		'set' => 'lithium\data\collection\DocumentSet',
+		'relationship' => 'lithium\data\model\Relationship',
+		'schema' => 'lithium\data\DocumentSchema'
+	);
+}
+
+?>
