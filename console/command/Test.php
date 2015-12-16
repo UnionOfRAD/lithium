32c2f2dfdfc3a560e01c104f94ce782aac77e583
Alex Bowers <alex@shopblocks.com>
2015-12-16 15:56:25 +0000

    Property needed for command help

--- console/command/Test.php
+++ console/command/Test.php
@@ -67,6 +67,13 @@ class Test extends \lithium\console\Command {
 	public $plain = false;
 
 	/**
+	 * Shows text output with color or without
+	 *
+	 * @var boolean
+	 */
+	public $noColor = false;
+
+	/**
 	 * An array of closures, mapped by type, which are set up to handle different test output
 	 * formats.
 	 *
