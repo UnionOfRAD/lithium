581bde24402e2f17331fb34f44bdcffc9fc5e487
David Persson <davidpersson@gmx.de>
2015-12-30 00:52:37 +0100

    Turn socket tests into integration tests.
    Loose dependency on Mocker, functions weren't reliably overloaded
    when running whole test suite. Partially reverts
    77af1d53f1f31e03eb39d0d2ae77192b3ff79a01.
    
    Fixes #1243
    Fixes #1244

--- tests/integration/net/socket/ContextTest.php
+++ tests/integration/net/socket/ContextTest.php
@@ -6,18 +6,17 @@
  * @license       http://opensource.org/licenses/bsd-license.php The BSD License
  */
 
-namespace lithium\tests\cases\net\socket;
+namespace lithium\tests\integration\net\socket;
 
 use lithium\net\http\Request;
 use lithium\net\socket\Context;
-use lithium\test\Mocker;
 
-class ContextTest extends \lithium\test\Unit {
+class ContextTest extends \lithium\test\Integration {
 
 	protected $_testConfig = array(
 		'persistent' => false,
 		'scheme' => 'http',
-		'host' => 'google.com',
+		'host' => 'example.org',
 		'port' => 80,
 		'timeout' => 4,
 		'classes' => array(
@@ -26,55 +25,8 @@ class ContextTest extends \lithium\test\Unit {
 		)
 	);
 
-	public function setUp() {
-		$base = 'lithium\net\socket';
-		$namespace = __NAMESPACE__;
-		Mocker::overwriteFunction("{$namespace}\\stream_context_get_options", function($resource) {
-			rewind($resource);
-			return unserialize(stream_get_contents($resource));
-		});
-		Mocker::overwriteFunction("{$base}\\stream_context_create", function($options) {
-			return $options;
-		});
-		Mocker::overwriteFunction("{$base}\\fopen", function($file, $mode, $includePath, $context) {
-			$handle = fopen("php://memory", "rw");
-			fputs($handle, serialize($context));
-			return $handle;
-		});
-		Mocker::overwriteFunction("{$base}\\stream_get_meta_data", function($resource) {
-			return array(
-				'wrapper_data' => array(
-					'HTTP/1.1 301 Moved Permanently',
-					'Location: http://www.google.com/',
-					'Content-Type: text/html; charset=UTF-8',
-					'Date: Thu, 28 Feb 2013 07:05:10 GMT',
-					'Expires: Sat, 30 Mar 2013 07:05:10 GMT',
-					'Cache-Control: public, max-age=2592000',
-					'Server: gws',
-					'Content-Length: 219',
-					'X-XSS-Protection: 1; mode=block',
-					'X-Frame-Options: SAMEORIGIN',
-					'Connection: close',
-				),
-			);
-		});
-		Mocker::overwriteFunction("{$base}\\stream_get_contents", function($resource) {
-			return <<<EOD
-<HTML><HEAD><meta http-equiv="content-type" content="text/html;charset=utf-8">
-<TITLE>301 Moved</TITLE></HEAD><BODY>
-<H1>301 Moved</H1>
-The document has moved
-<A HREF="http://www.google.com/">here</A>.
-</BODY></HTML>
-EOD;
-		});
-		Mocker::overwriteFunction("{$base}\\feof", function($resource) {
-			return true;
-		});
-	}
-
-	public function tearDown() {
-		Mocker::overwriteFunction(false);
+	public function skip() {
+		$this->skipIf(!$this->_hasNetwork(), 'No network connection.');
 	}
 
 	public function testConstruct() {
@@ -170,12 +122,12 @@ EOD;
 		$response = $socket->send();
 		$this->assertInstanceOf('lithium\net\http\Response', $response);
 
-		$expected = 'google.com';
+		$expected = 'example.org';
 		$result = $response->host;
 		$this->assertEqual($expected, $result);
 
 		$result = $response->body();
-		$this->assertPattern("/<title[^>]*>301 Moved<\/title>/im", (string) $result);
+		$this->assertPattern("/<title[^>]*>Example Domain<\/title>/im", (string) $result);
 	}
 }
 
