175a0189c8a8422fec6d56dfcdba906557299e49
David Persson <davidpersson@gmx.de>
2016-01-04 11:31:05 +0100

    Fix adaptable to use static access.

--- tests/cases/core/AdaptableTest.php
+++ tests/cases/core/AdaptableTest.php
@@ -57,41 +57,38 @@ class AdaptableTest extends \lithium\test\Unit {
 	}
 
 	public function testNonExistentConfig() {
-		$adapter = new MockAdaptable();
-		$this->assertException("Configuration `non_existent_config` has not been defined.", function() use ($adapter) {
-			$adapter::adapter('non_existent_config');
+		$this->assertException("Configuration `non_existent_config` has not been defined.", function() {
+			MockAdaptable::adapter('non_existent_config');
 		});
 	}
 
 	public function testAdapter() {
-		$adapter = new MockAdaptable();
 		$items = array('default' => array('adapter' => 'Memory', 'filters' => array()));
-		$adapter::config($items);
-		$result = $adapter::config();
+		MockAdaptable::config($items);
+		$result = MockAdaptable::config();
 		$expected = $items;
 		$this->assertEqual($expected, $result);
 
-		$result = $adapter::adapter('default');
+		$result = MockAdaptable::adapter('default');
 		$expected = new Memory($items['default']);
 		$this->assertEqual($expected, $result);
 	}
 
 	public function testConfigAndAdapter() {
-		$adapter = new MockAdaptable();
 		$items = array('default' => array('adapter' => 'Memory', 'filters' => array()));
-		$adapter::config($items);
-		$config = $adapter::config();
+		MockAdaptable::config($items);
+		$config = MockAdaptable::config();
 
-		$intermediate = $adapter::adapter('default');
+		$intermediate = MockAdaptable::adapter('default');
 		$expected = new Memory($items['default']);
 		$this->assertEqual($expected, $intermediate);
 
-		$result = $adapter::config();
+		$result = MockAdaptable::config();
 		$modified['default'] = $config['default'] + array('object' => $intermediate);
 		$this->assertEqual($modified, $result);
 
-		$adapter::config(array('default' => array('adapter' => 'Memory')));
-		$result = $adapter::config();
+		MockAdaptable::config(array('default' => array('adapter' => 'Memory')));
+		$result = MockAdaptable::config();
 		$this->assertEqual($config, $result);
 	}
 
@@ -263,69 +260,63 @@ class AdaptableTest extends \lithium\test\Unit {
 	}
 
 	public function testEnabled() {
-		$adapter = new MockAdaptable();
-
 		$items = array('default' => array('adapter' => 'Memory', 'filters' => array()));
-		$adapter::config($items);
-		$result = $adapter::config();
+		MockAdaptable::config($items);
+		$result = MockAdaptable::config();
 		$expected = $items;
 		$this->assertEqual($expected, $result);
 
-		$result = $adapter::adapter('default');
+		$result = MockAdaptable::adapter('default');
 		$expected = new Memory($items['default']);
 		$this->assertEqual($expected, $result);
 
-		$this->assertIdentical(true, $adapter::enabled('default'));
-		$this->assertException('/No adapter set for configuration/', function() use ($adapter) {
-			$adapter::enabled('non-existent');
+		$this->assertIdentical(true, MockAdaptable::enabled('default'));
+		$this->assertException('/No adapter set for configuration/', function() {
+			MockAdaptable::enabled('non-existent');
 		});
 	}
 
 	public function testNonExistentAdapter() {
-		$adapter = new MockAdaptable();
-
 		$items = array('default' => array('adapter' => 'NonExistent', 'filters' => array()));
-		$adapter::config($items);
-		$result = $adapter::config();
+		MockAdaptable::config($items);
+		$result = MockAdaptable::config();
 		$expected = $items;
 		$this->assertEqual($expected, $result);
 
 		$message  = 'Could not find adapter `NonExistent` in ';
 		$message .= 'class `lithium\tests\mocks\core\MockAdaptable`.';
-		$this->assertException($message, function() use ($adapter) {
-			$adapter::adapter('default');
+		$this->assertException($message, function() {
+			MockAdaptable::adapter('default');
 		});
 	}
 
 	public function testEnvironmentSpecificConfiguration() {
-		$adapter = new MockAdaptable();
 		$config = array('adapter' => 'Memory', 'filters' => array());
 		$items = array('default' => array(
 			'development' => $config, 'test' => $config, 'production' => $config
 		));
-		$adapter::config($items);
-		$result = $adapter::config();
+		MockAdaptable::config($items);
+		$result = MockAdaptable::config();
 		$expected = array('default' => $config);
 		$this->assertEqual($expected, $result);
 
-		$result = $adapter::config('default');
+		$result = MockAdaptable::config('default');
 		$expected = $config;
 		$this->assertEqual($expected, $result);
 
-		$result = $adapter::adapter('default');
+		$result = MockAdaptable::adapter('default');
 		$expected = new Memory($config);
 		$this->assertEqual($expected, $result);
 	}
 
 	public function testConfigurationNoAdapter() {
-		$adapter = new MockAdaptable();
 		$items = array('default' => array('filters' => array()));
-		$adapter::config($items);
+		MockAdaptable::config($items);
 
 		$message  = 'No adapter set for configuration in ';
 		$message .= 'class `lithium\tests\mocks\core\MockAdaptable`.';
-		$this->assertException($message, function() use ($adapter) {
-			$adapter::adapter('default');
+		$this->assertException($message, function() {
+			MockAdaptable::adapter('default');
 		});
 	}
 
