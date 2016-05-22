b8f908123162e19e9989722f0675ab9c7628fd00
David Persson <davidpersson@gmx.de>
2016-05-22 19:15:11 +0200

    More 2016.

--- console/li3.bat
+++ console/li3.bat
@@ -2,7 +2,7 @@
 rem
 rem Lithium: the most rad php framework
 rem
-rem @copyright     Copyright 2015, Union of RAD (http://union-of-rad.org)
+rem @copyright     Copyright 2016, Union of RAD (http://union-of-rad.org)
 rem @license       http://opensource.org/licenses/bsd-license.php The BSD License
 rem
 php -f "%~dp0lithium.php" %*
