2106bba286bfc152ea94d1fa1ee54e9b40dded64
David Persson <davidpersson@gmx.de>
2016-08-26 15:25:32 +0200

    Add test to prove readPref before client connection.
    Refs #1279.

--- tests/mocks/core/MockCallable.php
+++ tests/mocks/core/MockCallable.php
@@ -16,13 +16,17 @@ class MockCallable extends \lithium\core\Object {
 
 	public $get = '';
 
+	public $trace = array();
+
 	public static $callStatic = array();
 
 	public function __construct() {
+		$this->trace[] = array(__FUNCTION__, func_get_args());
 		$this->construct = func_get_args();
 	}
 
 	public function __call($method, $params = array()) {
+		$this->trace[] = array(__FUNCTION__, func_get_args());
 		return $this->call = compact('method', 'params');
 	}
 
@@ -31,6 +35,7 @@ class MockCallable extends \lithium\core\Object {
 	}
 
 	public function __get($value) {
+		$this->trace[] = array(__FUNCTION__, func_get_args());
 		return $this->get = $value;
 	}
 }
