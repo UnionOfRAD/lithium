4af3ad3b13a1c2488657212c4a5591feb80a574e
David Persson <davidpersson@gmx.de>
2017-04-28 10:54:11 +0200

    Reset copyright year to creation date of each file.
    This stops the madness of bumping the copyright year
    each new year, potentially requring updates to otherwise
    untouched versions.

--- console/li3.bat
+++ console/li3.bat
@@ -2,7 +2,7 @@
 rem
 rem li₃: the most RAD framework for PHP (http://li3.me)
 rem
-rem Copyright 2016, Union of RAD. All rights reserved. This source 
+rem Copyright 2009, Union of RAD. All rights reserved. This source 
 rem code is distributed under the terms of the BSD 3-Clause License. 
 rem The full license text can be found in the LICENSE.txt file.
 rem
--- tests/mocks/aop/MockInstanceFiltered.php
+++ tests/mocks/aop/MockInstanceFiltered.php
@@ -2,7 +2,7 @@
 /**
  * li₃: the most RAD framework for PHP (http://li3.me)
  *
- * Copyright 2016, Union of RAD. All rights reserved. This source
+ * Copyright 2015, Union of RAD. All rights reserved. This source
  * code is distributed under the terms of the BSD 3-Clause License.
  * The full license text can be found in the LICENSE.txt file.
  */
