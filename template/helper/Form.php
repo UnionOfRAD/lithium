150ab62322f2bbf927c35903c54bcecb7fd2367f
David Persson <nperson@gmx.de>
2017-10-27 18:12:12 +0200

    Use short array in docblock.

--- template/helper/Form.php
+++ template/helper/Form.php
@@ -439,7 +439,7 @@ class Form extends \lithium\template\Helper {
 	 *          parameters. By default, the label text is a human-friendly version of `$name`.
 	 *          However, you can specify the label manually as a string, or both the label
 	 *          text and options as an array, i.e.:
-	 *          `array('Your Label Title' => ['class' => 'foo', 'other' => 'options'])`.
+	 *          `['Your Label Title' => ['class' => 'foo', 'other' => 'options']]`.
 	 *        - `'type'` _string_: The type of form field to render. Available default options
 	 *          are: `'text'`, `'textarea'`, `'select'`, `'checkbox'`, `'password'` or
 	 *          `'hidden'`, as well as any arbitrary type (i.e. HTML5 form fields).
