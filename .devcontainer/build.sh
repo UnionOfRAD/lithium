72159150495559297551c0514a5cff2fb386bfcc
Jeff Loiselle <jeff.loiselle@gmail.com>
2022-01-19 17:46:51 +0000

    adds ssl for mongo

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 apt-get update && export DEBIAN_FRONTEND=noninteractive \
-    && apt-get -y install --no-install-recommends curl default-mysql-client git libicu-dev libmemcached-dev libmcrypt-dev libpq-dev netcat postgresql-client-13 vim zip zlib1g-dev
+    && apt-get -y install --no-install-recommends curl default-mysql-client git libicu-dev libmemcached-dev libmcrypt-dev libpq-dev libssl-dev netcat postgresql-client-13 vim zip zlib1g-dev
 
 pecl install apcu mcrypt memcached mongodb opcache redis xdebug
 docker-php-ext-install intl pdo pdo_mysql pdo_pgsql
