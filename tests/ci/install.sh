4897c09563d3cef77dc700731608905f3326603f
Nate Abele <nate@radify.io>
2022-11-18 20:13:55 +0000

    Check / drop databases before CI run

--- tests/ci/install.sh
+++ tests/ci/install.sh
@@ -1,22 +1,16 @@
 #/bin/bash
 set -xeuf
 
-cd ..
-cp -r lithium/tests/ci/app .
-if [ ! -e "app/libraries/lithium" ] ; then
-    ln -s ../../lithium app/libraries/lithium
-fi
-
-cd app
-
-set +e
-
 mkdir -p /tmp/tmp/logs
 mkdir -p /tmp/tmp/tests
 mkdir -p /tmp/tmp/cache/templates
-mysql -h mysql -u root -ppassword -e 'create database lithium_test; create database lithium_test_alternative;'
+mysql -h mysql -u root -ppassword -e 'drop database if exists lithium_test; create database lithium_test;'
+mysql -h mysql -u root -ppassword -e 'drop database if exists lithium_test_alternative; create database lithium_test_alternative;'
 
+psql postgresql://postgres:5432 -U postgres -c 'drop database if exists lithium_test;'
 psql postgresql://postgres:5432 -U postgres -c 'create database lithium_test;' 
+psql postgresql://postgres:5432 -U postgres -c 'drop database if exists lithium_test_alternative;' 
 psql postgresql://postgres:5432 -U postgres -c 'create database lithium_test_alternative;' 
 
+curl -X DELETE http://couchdb:5984/lithium_test/
 curl -X PUT http://couchdb:5984/lithium_test/
